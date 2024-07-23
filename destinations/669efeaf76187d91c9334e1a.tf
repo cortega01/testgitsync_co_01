7ba79956e76ed5937d892c31858a682e7f230fe5
Segment Git Sync <friends@segment.com>
2024-07-23 00:52:00 +0000

    Destination '669efeaf76187d91c9334e1a' created by cortega@twilio.com

---
+++ destinations/669efeaf76187d91c9334e1a.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-669efeaf76187d91c9334e1a
+  id = "669efeaf76187d91c9334e1a"
+}
+
+resource "segment_destination" "id-669efeaf76187d91c9334e1a" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "645babd9362d97b777391325"
+    partner_owned     = true
+    region_endpoints  = null
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Iterable (Actions)"
+  settings  = "{\"apiKey\":\"\",\"dataCenterLocation\":\"united_states\"}"
+  source_id = "7U1pz1W1ESiasVbLSGWWzT"
+}
