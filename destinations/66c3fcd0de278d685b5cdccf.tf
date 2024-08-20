307be7f2aa1053cc17da93071c5d89e897bb206c
Segment Git Sync <friends@segment.com>
2024-08-20 02:17:53 +0000

    Destination '66c3fcd0de278d685b5cdccf' created by cortega@twilio.com

---
+++ destinations/66c3fcd0de278d685b5cdccf.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66c3fcd0de278d685b5cdccf
+  id = "66c3fcd0de278d685b5cdccf"
+}
+
+resource "segment_destination" "id-66c3fcd0de278d685b5cdccf" {
+  enabled = true
+  metadata = {
+    contacts          = null
+    id                = "645babd9362d97b777391325"
+    partner_owned     = true
+    region_endpoints  = null
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "rETL Iterable Actions"
+  settings  = "{\"apiKey\":\"••••••••••fa72\",\"dataCenterLocation\":\"united_states\",\"enabled\":false,\"name\":\"\"}"
+  source_id = "MXozrV9sTShzR173cWRTD"
+}
