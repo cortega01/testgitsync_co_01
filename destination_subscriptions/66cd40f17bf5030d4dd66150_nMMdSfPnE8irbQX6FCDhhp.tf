38f562ef2fcd455f774ed50c93a2085dac51ce24
Segment Git Sync <friends@segment.com>
2024-08-27 04:11:25 +0000

    Destination Subscription '66cd40f17bf5030d4dd66150:nMMdSfPnE8irbQX6FCDhhp' created by cortega@twilio.com

---
+++ destination_subscriptions/66cd40f17bf5030d4dd66150_nMMdSfPnE8irbQX6FCDhhp.tf
@@ -0,0 +1,14 @@
+import {
+  to = segment_destination_subscription.id-66cd40f17bf5030d4dd66150_nMMdSfPnE8irbQX6FCDhhp
+  id = "66cd40f17bf5030d4dd66150:nMMdSfPnE8irbQX6FCDhhp"
+}
+
+resource "segment_destination_subscription" "id-66cd40f17bf5030d4dd66150_nMMdSfPnE8irbQX6FCDhhp" {
+  action_id      = "dfT2tG36Ddr8MUKCUvcvLt"
+  destination_id = "66cd40f17bf5030d4dd66150"
+  enabled        = false
+  model_id       = "jz6zAKGNMDyLsriPSKU7Z1"
+  name           = "Upsert Company"
+  settings       = "{}"
+  trigger        = "type = \"group\""
+}
