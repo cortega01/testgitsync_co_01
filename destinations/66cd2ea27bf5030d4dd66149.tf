d1921df2641241e775c6021a9190d7379ab6a454
Segment Git Sync <friends@segment.com>
2024-08-27 01:40:51 +0000

    Destination '66cd2ea27bf5030d4dd66149' created by cortega@twilio.com

---
+++ destinations/66cd2ea27bf5030d4dd66149.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66cd2ea27bf5030d4dd66149
+  id = "66cd2ea27bf5030d4dd66149"
+}
+
+resource "segment_destination" "id-66cd2ea27bf5030d4dd66149" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "632b1116e0cb83902f3fd717"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Hubspot Cloud Mode (Actions) HTTP Test 1"
+  settings  = "{\"portalId\":\"\"}"
+  source_id = "6PtA2kfhX5eMPqPQjjMgTx"
+}
