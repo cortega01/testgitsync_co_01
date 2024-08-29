daf1921eff56e943185d90ec75da3740cf6284c5
Segment Git Sync <friends@segment.com>
2024-08-29 08:26:05 +0000

    Destination '66d0309c0e972329c6bdbee5' created by cortega@twilio.com

---
+++ destinations/66d0309c0e972329c6bdbee5.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66d0309c0e972329c6bdbee5
+  id = "66d0309c0e972329c6bdbee5"
+}
+
+resource "segment_destination" "id-66d0309c0e972329c6bdbee5" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "54521fd525e721e32a72eeac"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name      = "Eloqua Destination Test JS"
+  settings  = "{\"companyName\":\"TechnologyPartnerSegmentio\",\"createContactOnTrack\":false,\"mappedEvents\":{},\"mappedGroupTraits\":{},\"mappedIdentifyTraits\":{},\"password\":\"••••••••••234!\",\"sendGroup\":false,\"siteId\":\"p02\",\"username\":\"Calvin.French-Owen\"}"
+  source_id = "f9XSryLD3S4oTyquhekAit"
+}
