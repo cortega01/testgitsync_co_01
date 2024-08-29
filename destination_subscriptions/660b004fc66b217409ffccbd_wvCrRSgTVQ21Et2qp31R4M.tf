c2d1b7a0c17e682715e8f42f64b21c2ac7999b83
Segment Git Sync <friends@segment.com>
2024-08-29 07:15:30 +0000

    Destination Subscription '660b004fc66b217409ffccbd:wvCrRSgTVQ21Et2qp31R4M' created by cortega@twilio.com

---
+++ destination_subscriptions/660b004fc66b217409ffccbd_wvCrRSgTVQ21Et2qp31R4M.tf
@@ -0,0 +1,14 @@
+import {
+  to = segment_destination_subscription.id-660b004fc66b217409ffccbd_wvCrRSgTVQ21Et2qp31R4M
+  id = "660b004fc66b217409ffccbd:wvCrRSgTVQ21Et2qp31R4M"
+}
+
+resource "segment_destination_subscription" "id-660b004fc66b217409ffccbd_wvCrRSgTVQ21Et2qp31R4M" {
+  action_id      = "oiFvX4YXwyaPVysi1t7mtz"
+  destination_id = "660b004fc66b217409ffccbd"
+  enabled        = false
+  model_id       = "jz6zAKGNMDyLsriPSKU7Z1"
+  name           = "Send Track"
+  settings       = "{}"
+  trigger        = "type = \"track\""
+}
