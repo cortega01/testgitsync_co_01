10b44f70c3ec5b74a4e36e14939c958fffd8ffe3
Segment Git Sync <friends@segment.com>
2024-08-29 05:23:20 +0000

    Destination Subscription '66cd40f17bf5030d4dd66150:ku1WtsKDzfG9QTNSCNsymF' created by cortega@twilio.com

---
+++ destination_subscriptions/66cd40f17bf5030d4dd66150_ku1WtsKDzfG9QTNSCNsymF.tf
@@ -0,0 +1,14 @@
+import {
+  to = segment_destination_subscription.id-66cd40f17bf5030d4dd66150_ku1WtsKDzfG9QTNSCNsymF
+  id = "66cd40f17bf5030d4dd66150:ku1WtsKDzfG9QTNSCNsymF"
+}
+
+resource "segment_destination_subscription" "id-66cd40f17bf5030d4dd66150_ku1WtsKDzfG9QTNSCNsymF" {
+  action_id      = "v3BPw5E3WcwKH4nngqta2i"
+  destination_id = "66cd40f17bf5030d4dd66150"
+  enabled        = false
+  model_id       = "jz6zAKGNMDyLsriPSKU7Z1"
+  name           = "Upsert Contact"
+  settings       = "{}"
+  trigger        = "type = \"identify\""
+}
