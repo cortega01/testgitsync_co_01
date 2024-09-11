1744f387849a8ed97ba8c53fa357c2440af6b469
Segment Git Sync <friends@segment.com>
2024-09-11 03:31:59 +0000

    Source '68Ymf3CBXBa5YC2kTKzkNs' created by cortega@twilio.com

---
+++ sources/68Ymf3CBXBa5YC2kTKzkNs.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-68Ymf3CBXBa5YC2kTKzkNs
+  id = "68Ymf3CBXBa5YC2kTKzkNs"
+}
+
+resource "segment_source" "id-68Ymf3CBXBa5YC2kTKzkNs" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "BQp3zGUgjV"
+  }
+  name     = "Entity Group: snowflake in spa_2JAiSpeDFhNt1oMXyNMXJQ"
+  settings = "{\"account\":\"ecb63470.prod3.us-west-2\",\"database\":\"CORTEGA_SANDBOX\",\"username\":\"CORTEGA\",\"warehouse\":\"SEGMENT_WAREHOUSE\"}"
+  slug     = "entity-group-snowflake-spa_2jaispedfhnt1omxynmxjq"
+}
