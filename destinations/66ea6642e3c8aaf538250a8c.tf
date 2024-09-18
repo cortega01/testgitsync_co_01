09fe1d67f93e7638e09ff2b491f3d7921337dc6d
Segment Git Sync <friends@segment.com>
2024-09-18 05:33:55 +0000

    Destination '66ea6642e3c8aaf538250a8c' created by cortega@twilio.com

---
+++ destinations/66ea6642e3c8aaf538250a8c.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66ea6642e3c8aaf538250a8c
+  id = "66ea6642e3c8aaf538250a8c"
+}
+
+resource "segment_destination" "id-66ea6642e3c8aaf538250a8c" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "614a3c7d791c91c41bae7599"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Webhook (Actions) Test 1"
+  settings  = "{\"sharedSecret\":\"\"}"
+  source_id = "hbHTwZCSYHvegNutaqq1wc"
+}
