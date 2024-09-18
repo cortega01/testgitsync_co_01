08831553e6f417985be8a74132cbb77d10cb5729
Segment Git Sync <friends@segment.com>
2024-09-18 01:20:51 +0000

    Destination '66ea2abcb10a85d855031ad2' modified by cortega@twilio.com

--- destinations/66ea2abcb10a85d855031ad2.tf
+++ destinations/66ea2abcb10a85d855031ad2.tf
@@ -13,6 +13,6 @@ resource "segment_destination" "id-66ea2abcb10a85d855031ad2" {
     supported_regions = ["us-west-2", "eu-west-1"]
   }
   name      = "FB CAPI HTTP TEST 1"
-  settings  = "{\"pixelId\":\"\",\"testEventCode\":\"\"}"
+  settings  = "{\"pixelId\":\"545342626773734\",\"testEventCode\":\"TEST13321\"}"
   source_id = "hbHTwZCSYHvegNutaqq1wc"
 }
