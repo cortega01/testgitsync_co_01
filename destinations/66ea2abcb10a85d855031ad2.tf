ea22c848aa05a5aab73e81c49aa85156e30d3467
Segment Git Sync <friends@segment.com>
2024-09-18 01:19:57 +0000

    Destination '66ea2abcb10a85d855031ad2' created by cortega@twilio.com

---
+++ destinations/66ea2abcb10a85d855031ad2.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66ea2abcb10a85d855031ad2
+  id = "66ea2abcb10a85d855031ad2"
+}
+
+resource "segment_destination" "id-66ea2abcb10a85d855031ad2" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "61806e472cd47ea1104885fc"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "FB CAPI HTTP TEST 1"
+  settings  = "{\"pixelId\":\"\",\"testEventCode\":\"\"}"
+  source_id = "hbHTwZCSYHvegNutaqq1wc"
+}
