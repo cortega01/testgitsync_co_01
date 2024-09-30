76d47af47ad2f09a0c20e520ac83ed2e2c8939ba
Segment Git Sync <friends@segment.com>
2024-09-30 22:27:33 +0000

    Source 'okdA2Cbyf2HK9ACmJFdY43' created by cortega@twilio.com

---
+++ sources/okdA2Cbyf2HK9ACmJFdY43.tf
@@ -0,0 +1,20 @@
+import {
+  to = segment_source.id-okdA2Cbyf2HK9ACmJFdY43
+  id = "okdA2Cbyf2HK9ACmJFdY43"
+}
+
+resource "segment_source" "id-okdA2Cbyf2HK9ACmJFdY43" {
+  enabled = true
+  labels = [
+    {
+      key   = "environment"
+      value = "dev"
+    },
+  ]
+  metadata = {
+    id = "U9mT0bPcI6"
+  }
+  name     = "Node Linked Cert"
+  settings = "{}"
+  slug     = "node_linked_cert"
+}
