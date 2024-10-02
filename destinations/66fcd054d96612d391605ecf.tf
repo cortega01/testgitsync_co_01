5d810ed0474d1d8213f87f732df830b5c940f18a
Segment Git Sync <friends@segment.com>
2024-10-02 04:47:17 +0000

    Destination '66fcd054d96612d391605ecf' created by cortega@twilio.com

---
+++ destinations/66fcd054d96612d391605ecf.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66fcd054d96612d391605ecf
+  id = "66fcd054d96612d391605ecf"
+}
+
+resource "segment_destination" "id-66fcd054d96612d391605ecf" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "5850d8b680412f644ff55df2"
+    partner_owned     = false
+    region_endpoints  = ["US", "EU"]
+    supported_regions = null
+  }
+  name      = "Repeater Test 1"
+  settings  = "{\"endpoint\":\"https://api.segment.io\",\"repeatKeys\":[]}"
+  source_id = "rb7ZExPsn1G3WJJfWReH7v"
+}
