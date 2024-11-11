7ab115f2059ce54b024bf74486aa55789aba37b1
Segment Git Sync <friends@segment.com>
2024-11-11 23:38:50 +0000

    Tracking Plan 'tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T' modified by cortega@twilio.com

--- tracking_plans/tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T.tf
+++ tracking_plans/tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T.tf
@@ -5,7 +5,7 @@ import {
 
 resource "segment_tracking_plan" "id-tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T" {
   description = "description"
-  name        = "Untitled Tracking Plan"
+  name        = "Tracking Plan Test"
   rules = [
     {
       json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"description\":\"DO NOT EDIT. Event generated to store end users consent preferences for Unify and Twilio Engage.\",\"properties\":{\"context\":{\"type\":\"object\"},\"properties\":{\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"type\":\"object\"}"
