27fdb039bc805eef56edcd319957b735163c60d3
Segment Git Sync <friends@segment.com>
2024-11-12 21:25:04 +0000

    Reverse ETL Model 'fkHiiYWFY1zDj6SGF1fo8N' created by cortega@twilio.com

---
+++ reverse_etl_models/fkHiiYWFY1zDj6SGF1fo8N.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_reverse_etl_model.id-fkHiiYWFY1zDj6SGF1fo8N
+  id = "fkHiiYWFY1zDj6SGF1fo8N"
+}
+
+resource "segment_reverse_etl_model" "id-fkHiiYWFY1zDj6SGF1fo8N" {
+  description             = "Programatically generated rETL model that is associated with an Engage rETL audience"
+  enabled                 = true
+  name                    = "Linked Growth Audience Test"
+  query                   = "SELECT 1"
+  query_identifier_column = "matchId"
+  schedule_config         = "{}"
+  schedule_strategy       = ""
+  source_id               = "68Ymf3CBXBa5YC2kTKzkNs"
+}
