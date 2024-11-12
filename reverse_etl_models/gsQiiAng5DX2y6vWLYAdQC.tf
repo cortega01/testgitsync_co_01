04b8911b31e80bfd2c20897fb4eafed9f49a9774
Segment Git Sync <friends@segment.com>
2024-11-12 12:07:40 +0000

    Reverse ETL Model 'gsQiiAng5DX2y6vWLYAdQC' created by cortega@twilio.com

---
+++ reverse_etl_models/gsQiiAng5DX2y6vWLYAdQC.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_reverse_etl_model.id-gsQiiAng5DX2y6vWLYAdQC
+  id = "gsQiiAng5DX2y6vWLYAdQC"
+}
+
+resource "segment_reverse_etl_model" "id-gsQiiAng5DX2y6vWLYAdQC" {
+  description             = "Programatically generated rETL model that is associated with an Engage rETL audience"
+  enabled                 = true
+  name                    = "Test LA 1"
+  query                   = "SELECT 1"
+  query_identifier_column = "matchId"
+  schedule_config         = "{}"
+  schedule_strategy       = ""
+  source_id               = "68Ymf3CBXBa5YC2kTKzkNs"
+}
