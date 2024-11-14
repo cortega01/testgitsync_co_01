b102066de7a4f73a66955d0b249c922c7f5fb2e5
Segment Git Sync <friends@segment.com>
2024-11-14 23:32:30 +0000

    Destination Subscription '65adcd5104bb6830be35cf30:vinDctaHARcJDeEzeQs4eD' modified by cortega@twilio.com

--- destination_subscriptions/65adcd5104bb6830be35cf30_vinDctaHARcJDeEzeQs4eD.tf
+++ destination_subscriptions/65adcd5104bb6830be35cf30_vinDctaHARcJDeEzeQs4eD.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-65adcd5104bb6830be35cf30_vinDctaHARcJDeEzeQs4eD" {
   action_id      = "ih1XmCi3dEQwnAHcMUBhx9"
   destination_id = "65adcd5104bb6830be35cf30"
-  enabled        = false
+  enabled        = true
   model_id       = null
   name           = "Send Event to Data Extension"
   settings       = "{\"batch_size\":10,\"enable_batching\":false,\"id\":{\"@path\":\"$.properties.details.mergedLimit\"},\"keys\":{\"anonymous_Id\":{\"@path\":\"$.anonymousId\"}},\"values\":{\"product_id\":{\"@path\":\"$.properties.details.product_id\"}}}"
