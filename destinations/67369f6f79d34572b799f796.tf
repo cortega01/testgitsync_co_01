39541ddb998fb514cd50dcff50e862682823dbaa
Segment Git Sync <friends@segment.com>
2024-11-15 01:10:08 +0000

    Destination '67369f6f79d34572b799f796' created by cortega@twilio.com

---
+++ destinations/67369f6f79d34572b799f796.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-67369f6f79d34572b799f796
+  id = "67369f6f79d34572b799f796"
+}
+
+resource "segment_destination" "id-67369f6f79d34572b799f796" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "60f9d0d048950c356be2e4da"
+    partner_owned     = false
+    region_endpoints  = ["US", "EU"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Linked Cert Braze Cloud Mode Actions"
+  settings  = "{\"api_key\":\"\",\"app_id\":\"\",\"endpoint\":\"https://rest.iad-01.braze.com\"}"
+  source_id = "okdA2Cbyf2HK9ACmJFdY43"
+}
