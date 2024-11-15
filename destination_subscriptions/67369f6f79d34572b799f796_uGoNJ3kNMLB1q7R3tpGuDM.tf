ab6556e350d4ce1ea62d7533501e84aaae48567a
Segment Git Sync <friends@segment.com>
2024-11-15 02:09:54 +0000

    Destination Subscription '67369f6f79d34572b799f796:uGoNJ3kNMLB1q7R3tpGuDM' modified by cortega@twilio.com

--- destination_subscriptions/67369f6f79d34572b799f796_uGoNJ3kNMLB1q7R3tpGuDM.tf
+++ destination_subscriptions/67369f6f79d34572b799f796_uGoNJ3kNMLB1q7R3tpGuDM.tf
@@ -10,5 +10,5 @@ resource "segment_destination_subscription" "id-67369f6f79d34572b799f796_uGoNJ3k
   model_id       = null
   name           = "LE|Track|Product Added to Wishlist"
   settings       = "{\"_update_existing_only\":false,\"batch_size\":75,\"braze_id\":{\"@path\":\"$.properties.braze_id\"},\"email\":{\"@if\":{\"else\":{\"@path\":\"$.properties.email\"},\"exists\":{\"@path\":\"$.context.traits.email\"},\"then\":{\"@path\":\"$.context.traits.email\"}}},\"enable_batching\":true,\"external_id\":{\"@path\":\"$.userId\"},\"name\":{\"@path\":\"$.event\"},\"properties\":{\"@path\":\"$.properties\"},\"time\":{\"@path\":\"$.receivedAt\"}}"
-  trigger        = "type = \"track\" and event != \"Order Completed\""
+  trigger        = "type = \"track\" and event = \"Product Added to Wishlist\""
 }
