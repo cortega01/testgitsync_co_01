5f565e63834cec0a2abb67b7b10a51bd1632c7fe
Segment Git Sync <friends@segment.com>
2024-11-19 03:48:59 +0000

    Tracking Plan 'tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T' modified by cortega@twilio.com

--- tracking_plans/tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T.tf
+++ tracking_plans/tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T.tf
@@ -20,7 +20,7 @@ resource "segment_tracking_plan" "id-tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T" {
       version     = 1
     },
     {
-      json_schema = "{\"properties\":{\"context\":{},\"properties\":{},\"traits\":{}}}"
+      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{},\"traits\":{}},\"required\":[\"blockIdentifyAndGroup\"],\"type\":\"object\"}"
       key         = null
       type        = "COMMON"
       version     = 1
