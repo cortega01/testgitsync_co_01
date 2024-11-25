dcc5291b2187bad087f0263634c946cb542f775e
Segment Git Sync <friends@segment.com>
2024-11-25 03:27:37 +0000

    Destination '6743ee6845bb606d1c4c51f9' modified by cortega@twilio.com

--- destinations/6743ee6845bb606d1c4c51f9.tf
+++ destinations/6743ee6845bb606d1c4c51f9.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-6743ee6845bb606d1c4c51f9" {
-  enabled = false
+  enabled = true
   metadata = {
     contacts          = null
     id                = "650bdf1a62fb34ef0a8058e1"
@@ -13,6 +13,6 @@ resource "segment_destination" "id-6743ee6845bb606d1c4c51f9" {
     supported_regions = ["us-west-2", "eu-west-1"]
   }
   name      = "Klaviyo Actions Test 1"
-  settings  = "{\"api_key\":\"\"}"
+  settings  = "{\"api_key\":\"••••••••••dbbd\"}"
   source_id = "6PtA2kfhX5eMPqPQjjMgTx"
 }
