c140852c26106d19b5c6994097acf822925a7656
Segment Git Sync <friends@segment.com>
2024-11-25 03:26:33 +0000

    Destination '6743ee6845bb606d1c4c51f9' created by cortega@twilio.com

---
+++ destinations/6743ee6845bb606d1c4c51f9.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-6743ee6845bb606d1c4c51f9
+  id = "6743ee6845bb606d1c4c51f9"
+}
+
+resource "segment_destination" "id-6743ee6845bb606d1c4c51f9" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "650bdf1a62fb34ef0a8058e1"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Klaviyo Actions Test 1"
+  settings  = "{\"api_key\":\"\"}"
+  source_id = "6PtA2kfhX5eMPqPQjjMgTx"
+}
