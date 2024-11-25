7c63e8fe2fbd4f85f74f8460caa58c627b5d5b28
Segment Git Sync <friends@segment.com>
2024-11-25 03:27:49 +0000

    Destination Subscription '6743ee6845bb606d1c4c51f9:drJ3Q9Y44wf97hQGd2beru' created by cortega@twilio.com

---
+++ destination_subscriptions/6743ee6845bb606d1c4c51f9_drJ3Q9Y44wf97hQGd2beru.tf
@@ -0,0 +1,14 @@
+import {
+  to = segment_destination_subscription.id-6743ee6845bb606d1c4c51f9_drJ3Q9Y44wf97hQGd2beru
+  id = "6743ee6845bb606d1c4c51f9:drJ3Q9Y44wf97hQGd2beru"
+}
+
+resource "segment_destination_subscription" "id-6743ee6845bb606d1c4c51f9_drJ3Q9Y44wf97hQGd2beru" {
+  action_id      = "e3hcjRJqFJAJYkAis7RpeV"
+  destination_id = "6743ee6845bb606d1c4c51f9"
+  enabled        = true
+  model_id       = null
+  name           = "Upsert Profile"
+  settings       = "{\"batch_size\":10000,\"email\":{\"@path\":\"$.traits.email\"},\"first_name\":{\"@path\":\"$.traits.firstName\"},\"image\":{\"@path\":\"$.traits.avatar\"},\"last_name\":{\"@path\":\"$.traits.lastName\"},\"location\":{\"address1\":{\"@path\":\"$.traits.address.street\"},\"city\":{\"@path\":\"$.traits.address.city\"},\"country\":{\"@path\":\"$.traits.address.country\"},\"region\":{\"@path\":\"$.traits.address.state\"},\"zip\":{\"@path\":\"$.traits.address.postal_code\"}},\"organization\":{\"@path\":\"$.traits.company.name\"},\"override_list_id\":{\"@path\":\"$.integrations.Klaviyo.listId\"},\"phone_number\":{\"@path\":\"$.context.traits.phone\"},\"properties\":{\"@path\":\"$.properties\"},\"title\":{\"@path\":\"$.traits.title\"}}"
+  trigger        = "type = \"identify\""
+}
