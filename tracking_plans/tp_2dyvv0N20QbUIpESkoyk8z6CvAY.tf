c7d2dd21236b9b9ebe56b4ad9c4fc2b71a7067cb
Segment Git Sync <friends@segment.com>
2024-11-29 01:11:50 +0000

    Tracking Plan 'tp_2dyvv0N20QbUIpESkoyk8z6CvAY' modified by cortega@twilio.com

---
+++ tracking_plans/tp_2dyvv0N20QbUIpESkoyk8z6CvAY.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_tracking_plan.id-tp_2dyvv0N20QbUIpESkoyk8z6CvAY
+  id = "tp_2dyvv0N20QbUIpESkoyk8z6CvAY"
+}
+
+resource "segment_tracking_plan" "id-tp_2dyvv0N20QbUIpESkoyk8z6CvAY" {
+  description = "description"
+  name        = "Event Library"
+  rules = [
+    {
+      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"type\":\"object\"}"
+      key         = "Button 2 Clicked"
+      type        = "TRACK"
+      version     = 1
+    },
+  ]
+  type = "RULE_LIBRARY"
+}
