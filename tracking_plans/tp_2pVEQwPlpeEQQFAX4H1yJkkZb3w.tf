289ad119698831c9d76263d06cb05dcf2a24fde2
Segment Git Sync <friends@segment.com>
2024-11-29 01:17:08 +0000

    Tracking Plan 'tp_2pVEQwPlpeEQQFAX4H1yJkkZb3w' modified by cortega@twilio.com

--- tracking_plans/tp_2pVEQwPlpeEQQFAX4H1yJkkZb3w.tf
+++ tracking_plans/tp_2pVEQwPlpeEQQFAX4H1yJkkZb3w.tf
@@ -8,7 +8,7 @@ resource "segment_tracking_plan" "id-tp_2pVEQwPlpeEQQFAX4H1yJkkZb3w" {
   name        = "E-Commerce"
   rules = [
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"type\":\"object\"},\"traits\":{}},\"type\":\"object\"}"
+      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"properties\":{\"promotion_button_text\":{\"description\":\"\",\"id\":\"/properties/properties/properties/promotion_button_text\",\"type\":\"string\"},\"promotion_creative\":{\"description\":\"\",\"id\":\"/properties/properties/properties/promotion_creative\",\"type\":\"string\"},\"url\":{\"description\":\"URL of the page\",\"id\":\"/properties/properties/properties/url\",\"type\":\"string\"}},\"type\":\"object\"},\"traits\":{}},\"type\":\"object\"}"
       key         = "Property Library Rule"
       type        = "TRACK"
       version     = 1
