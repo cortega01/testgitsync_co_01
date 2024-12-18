948aa07630cefd2a4f8e1c4ff32277b529dc1e20
Segment Git Sync <friends@segment.com>
2024-12-18 03:46:47 +0000

    Tracking Plan 'tp_2dyu98FZaICb9fIodsiiGfdR9OM' modified by cortega@twilio.com

--- tracking_plans/tp_2dyu98FZaICb9fIodsiiGfdR9OM.tf
+++ tracking_plans/tp_2dyu98FZaICb9fIodsiiGfdR9OM.tf
@@ -8,28 +8,119 @@ resource "segment_tracking_plan" "id-tp_2dyu98FZaICb9fIodsiiGfdR9OM" {
   name        = "Test Tracking Plan"
   rules = [
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"description\":\"DO NOT EDIT. Event generated to store end users consent preferences for Unify and Twilio Engage.\",\"properties\":{\"context\":{\"type\":\"object\"},\"properties\":{\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"type\":\"object\"}"
-      key         = "Segment Consent Preference Updated"
-      type        = "TRACK"
-      version     = 1
+      json_schema = jsonencode({
+        "$schema"   = "http://json-schema.org/draft-07/schema#"
+        description = "DO NOT EDIT. Event generated to store end users consent preferences for Unify and Twilio Engage."
+        properties = {
+          context = {
+            type = "object"
+          }
+          properties = {
+            type = "object"
+          }
+          traits = {
+            type = "object"
+          }
+        }
+        type = "object"
+      })
+      key     = "Segment Consent Preference Updated"
+      type    = "TRACK"
+      version = 1
     },
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"description\":\"When user clicks on button\",\"properties\":{\"context\":{},\"properties\":{\"properties\":{\"button_type\":{\"description\":\"Type of button\",\"id\":\"/properties/properties/properties/button_type\",\"type\":\"string\"}},\"required\":[\"button_type\"],\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"required\":[\"properties\"],\"type\":\"object\"}"
-      key         = "Button Clicked"
-      type        = "TRACK"
-      version     = 1
+      json_schema = jsonencode({
+        "$schema"   = "http://json-schema.org/draft-07/schema#"
+        description = "When user clicks on button"
+        properties = {
+          context = {}
+          properties = {
+            properties = {
+              button_type = {
+                description = "Type of button"
+                id          = "/properties/properties/properties/button_type"
+                type        = "string"
+              }
+            }
+            required = ["button_type"]
+            type     = "object"
+          }
+          traits = {
+            type = "object"
+          }
+        }
+        required = ["properties"]
+        type     = "object"
+      })
+      key     = "Button Clicked"
+      type    = "TRACK"
+      version = 1
     },
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"properties\":{\"url\":{\"description\":\"URL of the page\",\"id\":\"/properties/properties/properties/url\",\"type\":\"string\"}},\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"type\":\"object\"}"
-      key         = "Promotion Clicked"
-      type        = "TRACK"
-      version     = 1
+      json_schema = jsonencode({
+        "$schema" = "http://json-schema.org/draft-07/schema#"
+        properties = {
+          context = {}
+          properties = {
+            properties = {
+              url = {
+                description = "URL of the page"
+                id          = "/properties/properties/properties/url"
+                type        = "string"
+              }
+            }
+            type = "object"
+          }
+          traits = {
+            type = "object"
+          }
+        }
+        type = "object"
+      })
+      key     = "Promotion Clicked"
+      type    = "TRACK"
+      version = 1
     },
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"type\":\"object\"}"
-      key         = "Button 2 Clicked"
-      type        = "TRACK"
-      version     = 1
+      json_schema = jsonencode({
+        "$schema" = "http://json-schema.org/draft-07/schema#"
+        properties = {
+          context = {}
+          properties = {
+            type = "object"
+          }
+          traits = {
+            type = "object"
+          }
+        }
+        type = "object"
+      })
+      key     = "Button 2 Clicked"
+      type    = "TRACK"
+      version = 1
+    },
+    {
+      json_schema = jsonencode({
+        "$schema" = "http://json-schema.org/draft-07/schema#"
+        properties = {
+          context    = {}
+          properties = {}
+          traits = {
+            properties = {
+              AllowGroupOrIdentify = {
+                type = "string"
+              }
+            }
+            required = ["AllowGroupOrIdentify"]
+            type     = "object"
+          }
+        }
+        required = ["traits"]
+        type     = "object"
+      })
+      key     = null
+      type    = "COMMON"
+      version = 1
     },
   ]
   type = "LIVE"
