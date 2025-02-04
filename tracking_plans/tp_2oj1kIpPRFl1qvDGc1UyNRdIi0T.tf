0e2e1d25b1ea438c0c458bbc12465237c859aa56
Segment Git Sync <friends@segment.com>
2025-02-04 02:58:58 +0000

    Tracking Plan 'tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T' modified by cortega@twilio.com

--- tracking_plans/tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T.tf
+++ tracking_plans/tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T.tf
@@ -5,25 +5,73 @@ import {
 
 resource "segment_tracking_plan" "id-tp_2oj1kIpPRFl1qvDGc1UyNRdIi0T" {
   description = "description"
-  name        = "Tracking Plan Test"
+  name        = "Linked Audiences TP Test"
   rules = [
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"description\":\"DO NOT EDIT. Event generated to store end users consent preferences for Unify and Twilio Engage.\",\"properties\":{\"context\":{\"type\":\"object\"},\"properties\":{\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"type\":\"object\"}"
-      key         = "Segment Consent Preference Updated"
-      type        = "TRACK"
-      version     = 1
+      json_schema = jsonencode({
+        "$schema"   = "http://json-schema.org/draft-07/schema#"
+        description = "DO NOT EDIT. Event generated to store end users consent preferences for Unify and Twilio Engage."
+        properties = {
+          context = {
+            type = "object"
+          }
+          properties = {
+            type = "object"
+          }
+          traits = {
+            type = "object"
+          }
+        }
+        type = "object"
+      })
+      key     = "Segment Consent Preference Updated"
+      type    = "TRACK"
+      version = 1
     },
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"description\":\"User submits form\",\"properties\":{\"context\":{},\"properties\":{\"properties\":{\"form_type\":{\"description\":\"Type of form (web, app)\",\"id\":\"/properties/properties/properties/form_type\",\"type\":\"string\"}},\"required\":[\"form_type\"],\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"required\":[\"properties\"],\"type\":\"object\"}"
-      key         = "Form Submitted"
-      type        = "TRACK"
-      version     = 1
+      json_schema = jsonencode({
+        "$schema"   = "http://json-schema.org/draft-07/schema#"
+        description = "User submits form"
+        properties = {
+          context = {}
+          properties = {
+            properties = {
+              form_type = {
+                description = "Type of form (web, app)"
+                id          = "/properties/properties/properties/form_type"
+                type        = "string"
+              }
+            }
+            required = ["form_type"]
+            type     = "object"
+          }
+          traits = {
+            type = "object"
+          }
+        }
+        required = ["properties"]
+        type     = "object"
+      })
+      key     = "Form Submitted"
+      type    = "TRACK"
+      version = 1
     },
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{},\"traits\":{\"required\":[\"AcceptGroupOrIdentify\"],\"type\":\"object\"}},\"type\":\"object\"}"
-      key         = null
-      type        = "COMMON"
-      version     = 1
+      json_schema = jsonencode({
+        "$schema" = "http://json-schema.org/draft-07/schema#"
+        properties = {
+          context    = {}
+          properties = {}
+          traits = {
+            required = ["AcceptGroupOrIdentify"]
+            type     = "object"
+          }
+        }
+        type = "object"
+      })
+      key     = null
+      type    = "COMMON"
+      version = 1
     },
   ]
   type = "LIVE"
