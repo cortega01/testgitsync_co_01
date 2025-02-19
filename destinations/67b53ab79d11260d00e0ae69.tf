4a49421a35b6f796301bb33f0fbad711fb8356df
Segment Git Sync <friends@segment.com>
2025-02-19 01:58:17 +0000

    Destination '67b53ab79d11260d00e0ae69' created by cortega@twilio.com

---
+++ destinations/67b53ab79d11260d00e0ae69.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_destination.id-67b53ab79d11260d00e0ae69
+  id = "67b53ab79d11260d00e0ae69"
+}
+
+resource "segment_destination" "id-67b53ab79d11260d00e0ae69" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "60f9d0d048950c356be2e4da"
+    partner_owned     = false
+    region_endpoints  = ["US", "EU"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "Braze HTTP API 1 (Development)"
+  settings = jsonencode({
+    api_key  = ""
+    app_id   = ""
+    endpoint = "https://rest.iad-01.braze.com"
+  })
+  source_id = "hbHTwZCSYHvegNutaqq1wc"
+}
