f504f3a0559d92faebf5ad9fc7334a5420b9f00d
Segment Git Sync <friends@segment.com>
2025-02-19 02:29:54 +0000

    Destination Subscription '465vmZAtKrxCQM5q536dju' modified by cortega@twilio.com

---
+++ destination_subscriptions/67b53ab79d11260d00e0ae69_465vmZAtKrxCQM5q536dju.tf
@@ -0,0 +1,71 @@
+import {
+  to = segment_destination_subscription.id-67b53ab79d11260d00e0ae69_465vmZAtKrxCQM5q536dju
+  id = "67b53ab79d11260d00e0ae69:465vmZAtKrxCQM5q536dju"
+}
+
+resource "segment_destination_subscription" "id-67b53ab79d11260d00e0ae69_465vmZAtKrxCQM5q536dju" {
+  action_id      = "2P24zUSAL8BUpyGYNGmD7M"
+  destination_id = "67b53ab79d11260d00e0ae69"
+  enabled        = true
+  model_id       = null
+  name           = "Identify Calls"
+  settings = jsonencode({
+    _update_existing_only = false
+    batch_size            = 75
+    braze_id = {
+      "@if" = {
+        else = {
+          "@path" = "$.traits.braze_id"
+        }
+        exists = {
+          "@template" = "{{integrations.Braze Cloud Mode (Actions).braze_id}}"
+        }
+        then = {
+          "@template" = "{{integrations.Braze Cloud Mode (Actions).braze_id}}"
+        }
+      }
+    }
+    country = {
+      "@path" = "$.context.location.country"
+    }
+    current_location = {
+      latitude = {
+        "@path" = "$.context.location.latitude"
+      }
+      longitude = {
+        "@path" = "$.context.location.longitude"
+      }
+    }
+    custom_attributes = {
+      subscription_groups = {
+        "@path" = "$.traits.sub_groups"
+      }
+    }
+    email = {
+      "@path" = "$.traits.email"
+    }
+    enable_batching = true
+    external_id = {
+      "@path" = "$.userId"
+    }
+    first_name = {
+      "@path" = "$.traits.firstName"
+    }
+    gender = {
+      "@path" = "$.traits.gender"
+    }
+    home_city = {
+      "@path" = "$.traits.address.city"
+    }
+    image_url = {
+      "@path" = "$.traits.avatar"
+    }
+    last_name = {
+      "@path" = "$.traits.lastName"
+    }
+    phone = {
+      "@path" = "$.traits.phone"
+    }
+  })
+  trigger = "type = \"identify\""
+}
