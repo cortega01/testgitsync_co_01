cb5b80dcd2f847e7a8caa7d9806f4686b8469f30
Segment Git Sync <friends@segment.com>
2025-02-26 02:44:02 +0000

    Destination '66ea2abcb10a85d855031ad2' modified by cortega@twilio.com

--- destinations/66ea2abcb10a85d855031ad2.tf
+++ destinations/66ea2abcb10a85d855031ad2.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-66ea2abcb10a85d855031ad2" {
-  enabled = false
+  enabled = true
   metadata = {
     contacts          = null
     id                = "61806e472cd47ea1104885fc"
@@ -12,7 +12,13 @@ resource "segment_destination" "id-66ea2abcb10a85d855031ad2" {
     region_endpoints  = ["US"]
     supported_regions = ["us-west-2", "eu-west-1"]
   }
-  name      = "FB CAPI HTTP TEST 1"
-  settings  = "{\"pixelId\":\"545342626773734\",\"testEventCode\":\"TEST13321\"}"
+  name = "FB CAPI HTTP TEST 1"
+  settings = jsonencode({
+    dynamicAuthSettings = {
+      configId = "66ea2abcb10a85d855031ad2"
+    }
+    pixelId       = "545342626773734"
+    testEventCode = "TEST13321"
+  })
   source_id = "hbHTwZCSYHvegNutaqq1wc"
 }
