5af83761451c6443874633b3c1abbf0cd6f8d9be
Segment Git Sync <friends@segment.com>
2025-02-26 02:03:02 +0000

    Destination Subscription 'mUAnoB7bqRdcUdK7JzzsQ6' modified by cortega@twilio.com

--- destination_subscriptions/66ea2abcb10a85d855031ad2_mUAnoB7bqRdcUdK7JzzsQ6.tf
+++ destination_subscriptions/66ea2abcb10a85d855031ad2_mUAnoB7bqRdcUdK7JzzsQ6.tf
@@ -4,11 +4,131 @@ import {
 }
 
 resource "segment_destination_subscription" "id-66ea2abcb10a85d855031ad2_mUAnoB7bqRdcUdK7JzzsQ6" {
-  action_id      = "8whh4kyqahLHgTJqUwvEwh"
-  destination_id = "66ea2abcb10a85d855031ad2"
-  enabled        = true
-  model_id       = null
-  name           = "Purchase"
-  settings       = "{\"action_source\":\"physical_store\",\"app_data_field\":{\"application_tracking_enabled\":{\"@path\":\"$.context.device.adTrackingEnabled\"},\"carrier\":{\"@path\":\"$.context.network.carrier\"},\"density\":{\"@path\":\"$.context.screen.density\"},\"deviceName\":{\"@path\":\"$.context.device.model\"},\"deviceTimezone\":{\"@path\":\"$.context.timezone\"},\"height\":{\"@path\":\"$.context.screen.height\"},\"locale\":{\"@path\":\"$.context.locale\"},\"longVersion\":{\"@path\":\"$.context.app.version\"},\"osVersion\":{\"@path\":\"$.context.os.version\"},\"packageName\":{\"@path\":\"$.context.app.namespace\"},\"width\":{\"@path\":\"$.context.screen.width\"}},\"contents\":{\"@arrayPath\":[{\"@path\":\"$.properties.products\"},{\"id\":{\"@path\":\"$.product_id\"},\"item_price\":{\"@path\":\"$.price\"},\"quantity\":{\"@path\":\"$.quantity\"}}]},\"currency\":{\"@path\":\"$.properties.currency\"},\"event_id\":{\"@path\":\"$.messageId\"},\"event_source_url\":{\"@path\":\"$.context.page.url\"},\"event_time\":{\"@path\":\"$.timestamp\"},\"user_data\":{\"city\":{\"@path\":\"$.context.traits.address.city\"},\"client_ip_address\":{\"@path\":\"$.context.ip\"},\"client_user_agent\":{\"@path\":\"$.context.userAgent\"},\"dateOfBirth\":{\"@path\":\"$.context.traits.birthday\"},\"email\":{\"@path\":\"$.context.traits.email\"},\"externalId\":{\"@if\":{\"else\":{\"@path\":\"$.anonymousId\"},\"exists\":{\"@path\":\"$.userId\"},\"then\":{\"@path\":\"$.userId\"}}},\"fbc\":{\"@path\":\"$.properties.fbc\"},\"fbp\":{\"@path\":\"$.properties.fbp\"},\"firstName\":{\"@path\":\"$.context.traits.firstName\"},\"lastName\":{\"@path\":\"$.context.traits.lastName\"},\"phone\":{\"@path\":\"$.context.traits.phone\"},\"state\":{\"@path\":\"$.context.traits.address.state\"},\"zip\":{\"@path\":\"$.context.traits.address.postalCode\"}},\"value\":{\"@path\":\"$.properties.revenue\"}}"
-  trigger        = "type = \"track\" and event = \"Order Completed\""
+  action_id            = "8whh4kyqahLHgTJqUwvEwh"
+  destination_id       = "66ea2abcb10a85d855031ad2"
+  enabled              = true
+  model_id             = null
+  name                 = "Purchase"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    action_source = "website"
+    app_data_field = {
+      application_tracking_enabled = {
+        "@path" = "$.context.device.adTrackingEnabled"
+      }
+      carrier = {
+        "@path" = "$.context.network.carrier"
+      }
+      density = {
+        "@path" = "$.context.screen.density"
+      }
+      deviceName = {
+        "@path" = "$.context.device.model"
+      }
+      deviceTimezone = {
+        "@path" = "$.context.timezone"
+      }
+      height = {
+        "@path" = "$.context.screen.height"
+      }
+      locale = {
+        "@path" = "$.context.locale"
+      }
+      longVersion = {
+        "@path" = "$.context.app.version"
+      }
+      osVersion = {
+        "@path" = "$.context.os.version"
+      }
+      packageName = {
+        "@path" = "$.context.app.namespace"
+      }
+      width = {
+        "@path" = "$.context.screen.width"
+      }
+    }
+    contents = {
+      "@arrayPath" = [{
+        "@path" = "$.properties.products"
+        }, {
+        id = {
+          "@path" = "$.product_id"
+        }
+        item_price = {
+          "@path" = "$.price"
+        }
+        quantity = {
+          "@path" = "$.quantity"
+        }
+      }]
+    }
+    currency = {
+      "@path" = "$.properties.currency"
+    }
+    event_id = {
+      "@path" = "$.properties.order_id"
+    }
+    event_source_url = {
+      "@path" = "$.context.page.url"
+    }
+    event_time = {
+      "@path" = "$.timestamp"
+    }
+    test_event_code = "TEST82273"
+    user_data = {
+      city = {
+        "@path" = "$.context.traits.address.city"
+      }
+      client_ip_address = {
+        "@path" = "$.context.ip"
+      }
+      client_user_agent = {
+        "@path" = "$.context.userAgent"
+      }
+      dateOfBirth = {
+        "@path" = "$.context.traits.birthday"
+      }
+      email = {
+        "@path" = "$.context.traits.email"
+      }
+      externalId = {
+        "@if" = {
+          else = {
+            "@path" = "$.anonymousId"
+          }
+          exists = {
+            "@path" = "$.userId"
+          }
+          then = {
+            "@path" = "$.userId"
+          }
+        }
+      }
+      fbc = {
+        "@path" = "$.properties.fbc"
+      }
+      fbp = {
+        "@path" = "$.properties.fbp"
+      }
+      firstName = {
+        "@path" = "$.context.traits.firstName"
+      }
+      lastName = {
+        "@path" = "$.context.traits.lastName"
+      }
+      phone = {
+        "@path" = "$.context.traits.phone"
+      }
+      state = {
+        "@path" = "$.context.traits.address.state"
+      }
+      zip = {
+        "@path" = "$.context.traits.address.postalCode"
+      }
+    }
+    value = {
+      "@path" = "$.properties.revenue"
+    }
+  })
+  trigger = "type = \"track\" and event = \"Order Completed\""
 }
