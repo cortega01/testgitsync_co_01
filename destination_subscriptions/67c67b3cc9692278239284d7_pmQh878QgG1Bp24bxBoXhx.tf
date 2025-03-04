8f689727238a0a5d8090c6a009fcbbbeb4ea5f7b
Segment Git Sync <friends@segment.com>
2025-03-04 04:02:13 +0000

    Destination Subscription 'pmQh878QgG1Bp24bxBoXhx' created by cortega@twilio.com

---
+++ destination_subscriptions/67c67b3cc9692278239284d7_pmQh878QgG1Bp24bxBoXhx.tf
@@ -0,0 +1,72 @@
+import {
+  to = segment_destination_subscription.id-67c67b3cc9692278239284d7_pmQh878QgG1Bp24bxBoXhx
+  id = "67c67b3cc9692278239284d7:pmQh878QgG1Bp24bxBoXhx"
+}
+
+resource "segment_destination_subscription" "id-67c67b3cc9692278239284d7_pmQh878QgG1Bp24bxBoXhx" {
+  action_id            = "2P24zUSAL8BUpyGYNGmD7M"
+  destination_id       = "67c67b3cc9692278239284d7"
+  enabled              = false
+  model_id             = null
+  name                 = "Newsletter Subscription | Brand 2"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    _update_existing_only = false
+    batch_size            = 75
+    braze_id = {
+      "@if" = {
+        else = {
+          "@path" = "$.traits.braze_id"
+        }
+        exists = {
+          "@template" = "{{integrations.Braze Cloud Mode (Actions).braze_id}}"
+        }
+        then = {
+          "@template" = "{{integrations.Braze Cloud Mode (Actions).braze_id}}"
+        }
+      }
+    }
+    country = {
+      "@path" = "$.context.location.country"
+    }
+    current_location = {
+      latitude = {
+        "@path" = "$.context.location.latitude"
+      }
+      longitude = {
+        "@path" = "$.context.location.longitude"
+      }
+    }
+    custom_attributes = {
+      subscription_groups = {
+        "@path" = "$.properties.promotion_type"
+      }
+    }
+    email = {
+      "@path" = "$.properties.email"
+    }
+    enable_batching = true
+    external_id = {
+      "@path" = "$.userId"
+    }
+    first_name = {
+      "@path" = "$.traits.firstName"
+    }
+    gender = {
+      "@path" = "$.traits.gender"
+    }
+    home_city = {
+      "@path" = "$.traits.address.city"
+    }
+    image_url = {
+      "@path" = "$.traits.avatar"
+    }
+    last_name = {
+      "@path" = "$.traits.lastName"
+    }
+    phone = {
+      "@path" = "$.traits.phone"
+    }
+  })
+  trigger = "type = \"track\" and event = \"Newsletter Subscription\""
+}
