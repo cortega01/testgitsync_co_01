5a90714f66de96e814e06615008b884c9d75c09c
Segment Git Sync <friends@segment.com>
2025-03-17 11:00:42 +0000

    Destination Subscription 'wRCe4Z3aeUPdEukZ9jibrA' modified by cortega@twilio.com

--- destination_subscriptions/66cd2f65ca46e7cc406c38ef_wRCe4Z3aeUPdEukZ9jibrA.tf
+++ destination_subscriptions/66cd2f65ca46e7cc406c38ef_wRCe4Z3aeUPdEukZ9jibrA.tf
@@ -4,11 +4,73 @@ import {
 }
 
 resource "segment_destination_subscription" "id-66cd2f65ca46e7cc406c38ef_wRCe4Z3aeUPdEukZ9jibrA" {
-  action_id      = "dfT2tG36Ddr8MUKCUvcvLt"
-  destination_id = "66cd2f65ca46e7cc406c38ef"
-  enabled        = true
-  model_id       = null
-  name           = "Upsert Company"
-  settings       = "{\"address\":{\"@path\":\"$.traits.address.street\"},\"associateContact\":true,\"city\":{\"@path\":\"$.traits.address.city\"},\"companysearchfields\":{\"name\":{\"@path\":\"$.traits.name\"}},\"createNewCompany\":true,\"description\":{\"@path\":\"$.traits.description\"},\"domain\":{\"@path\":\"$.traits.website\"},\"groupid\":{\"@if\":{\"else\":{\"@path\":\"$.context.groupId\"},\"exists\":{\"@path\":\"$.groupId\"},\"then\":{\"@path\":\"$.groupId\"}}},\"industry\":{\"@path\":\"$.traits.industry\"},\"name\":{\"@path\":\"$.traits.name\"},\"numberofemployees\":{\"@path\":\"$.traits.employees\"},\"phone\":{\"@path\":\"$.traits.phone\"},\"state\":{\"@path\":\"$.traits.address.state\"},\"zip\":{\"@if\":{\"else\":{\"@path\":\"$.traits.address.postal_code\"},\"exists\":{\"@path\":\"$.traits.address.postalCode\"},\"then\":{\"@path\":\"$.traits.address.postalCode\"}}}}"
-  trigger        = "type = \"group\""
+  action_id            = "dfT2tG36Ddr8MUKCUvcvLt"
+  destination_id       = "66cd2f65ca46e7cc406c38ef"
+  enabled              = false
+  model_id             = null
+  name                 = "Upsert Company"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    address = {
+      "@path" = "$.traits.address.street"
+    }
+    associateContact = true
+    city = {
+      "@path" = "$.traits.address.city"
+    }
+    companysearchfields = {
+      name = {
+        "@path" = "$.traits.name"
+      }
+    }
+    createNewCompany = true
+    description = {
+      "@path" = "$.traits.description"
+    }
+    domain = {
+      "@path" = "$.traits.website"
+    }
+    groupid = {
+      "@if" = {
+        else = {
+          "@path" = "$.context.groupId"
+        }
+        exists = {
+          "@path" = "$.groupId"
+        }
+        then = {
+          "@path" = "$.groupId"
+        }
+      }
+    }
+    industry = {
+      "@path" = "$.traits.industry"
+    }
+    name = {
+      "@path" = "$.traits.name"
+    }
+    numberofemployees = {
+      "@path" = "$.traits.employees"
+    }
+    phone = {
+      "@path" = "$.traits.phone"
+    }
+    state = {
+      "@path" = "$.traits.address.state"
+    }
+    zip = {
+      "@if" = {
+        else = {
+          "@path" = "$.traits.address.postal_code"
+        }
+        exists = {
+          "@path" = "$.traits.address.postalCode"
+        }
+        then = {
+          "@path" = "$.traits.address.postalCode"
+        }
+      }
+    }
+  })
+  trigger = "type = \"group\""
 }
