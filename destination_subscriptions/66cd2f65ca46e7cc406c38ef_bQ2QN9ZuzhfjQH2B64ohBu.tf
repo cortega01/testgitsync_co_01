c86468cf77189583df70da5ce8e2ddf69e413d03
Segment Git Sync <friends@segment.com>
2025-03-17 11:05:08 +0000

    Destination Subscription 'bQ2QN9ZuzhfjQH2B64ohBu' modified by cortega@twilio.com

--- destination_subscriptions/66cd2f65ca46e7cc406c38ef_bQ2QN9ZuzhfjQH2B64ohBu.tf
+++ destination_subscriptions/66cd2f65ca46e7cc406c38ef_bQ2QN9ZuzhfjQH2B64ohBu.tf
@@ -6,72 +6,72 @@ import {
 resource "segment_destination_subscription" "id-66cd2f65ca46e7cc406c38ef_bQ2QN9ZuzhfjQH2B64ohBu" {
   action_id            = "v3BPw5E3WcwKH4nngqta2i"
   destination_id       = "66cd2f65ca46e7cc406c38ef"
-  enabled              = false
+  enabled              = true
   model_id             = null
   name                 = "Upsert Contact (Copy)"
   reverse_etl_schedule = null
   settings = jsonencode({
     address = {
-      "@template" = "{{traits.address.street}}"
+      "@path" = "$.traits.address.street"
     }
     city = {
-      "@template" = "{{traits.address.city}}"
+      "@path" = "$.traits.address.city"
     }
     company = {
-      "@template" = "{{traits.company}}"
+      "@path" = "$.traits.company"
     }
     country = {
-      "@template" = "{{traits.address.country}}"
+      "@path" = "$.traits.address.country"
     }
     email = {
-      "@template" = "{{traits.email}}"
+      "@path" = "$.traits.email"
     }
     enable_batching = false
     firstname = {
       "@if" = {
         else = {
-          "@template" = "{{traits.firstName}}"
+          "@path" = "$.traits.firstName"
         }
         exists = {
-          "@template" = "{{traits.first_name}}"
+          "@path" = "$.traits.first_name"
         }
         then = {
-          "@template" = "{{traits.first_name}}"
+          "@path" = "$.traits.first_name"
         }
       }
     }
     lastname = {
       "@if" = {
         else = {
-          "@template" = "{{traits.lastName}}"
+          "@path" = "$.traits.lastName"
         }
         exists = {
-          "@template" = "{{traits.last_name}}"
+          "@path" = "$.traits.last_name"
         }
         then = {
-          "@template" = "{{traits.last_name}}"
+          "@path" = "$.traits.last_name"
         }
       }
     }
     phone = {
-      "@template" = "{{traits.phone}}"
+      "@path" = "$.traits.phone"
     }
     state = {
-      "@template" = "{{traits.address.state}}"
+      "@path" = "$.traits.address.state"
     }
     website = {
-      "@template" = "{{traits.website}}"
+      "@path" = "$.traits.website"
     }
     zip = {
       "@if" = {
         else = {
-          "@template" = "{{traits.address.postal_code}}"
+          "@path" = "$.traits.address.postal_code"
         }
         exists = {
-          "@template" = "{{traits.address.postalCode}}"
+          "@path" = "$.traits.address.postalCode"
         }
         then = {
-          "@template" = "{{traits.address.postalCode}}"
+          "@path" = "$.traits.address.postalCode"
         }
       }
     }
