46e242316152edca1a6ee674ac622da16aa76400
Segment Git Sync <friends@segment.com>
2025-04-09 02:11:44 +0000

    Destination '67f5d7023960047bb9c11eb5' modified by cortega@twilio.com

--- destinations/67f5d7023960047bb9c11eb5.tf
+++ destinations/67f5d7023960047bb9c11eb5.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-67f5d7023960047bb9c11eb5" {
-  enabled = false
+  enabled = true
   metadata = {
     contacts          = null
     id                = "615c7438d93d9b61b1e9e192"
@@ -14,10 +14,13 @@ resource "segment_destination" "id-67f5d7023960047bb9c11eb5" {
   }
   name = "Mixpanel Actions Test 1"
   settings = jsonencode({
-    apiRegion    = "US 🇺🇸"
-    apiSecret    = ""
-    projectToken = ""
-    sourceName   = ""
+    apiRegion = "US 🇺🇸"
+    apiSecret = "••••••••••b88d"
+    dynamicAuthSettings = {
+      configId = "67f5d7023960047bb9c11eb5"
+    }
+    projectToken = "a246e1e8d3bad6f805974acc9ef163b7"
+    sourceName   = "http_test_co_1"
     strictMode   = "1"
   })
   source_id = "hbHTwZCSYHvegNutaqq1wc"
