6e51598a823b17521da46bcedb8d85a8942ed411
Segment Git Sync <friends@segment.com>
2025-04-28 08:19:44 +0000

    Destination '67b53ab79d11260d00e0ae69' modified by cortega@twilio.com

--- destinations/67b53ab79d11260d00e0ae69.tf
+++ destinations/67b53ab79d11260d00e0ae69.tf
@@ -14,7 +14,7 @@ resource "segment_destination" "id-67b53ab79d11260d00e0ae69" {
   }
   name = "Braze HTTP API 1 (Development)"
   settings = jsonencode({
-    api_key = "••••••••••a387"
+    api_key = "••••••••••c23c"
     app_id  = ""
     dynamicAuthSettings = {
       configId = "67b53ab79d11260d00e0ae69"
