604eec1eb4a550138372addc258ede11a8fb4b74
Segment Git Sync <friends@segment.com>
2025-04-28 08:12:37 +0000

    Destination '6736a10b303d062c7fe69f0a' modified by cortega@twilio.com

--- destinations/6736a10b303d062c7fe69f0a.tf
+++ destinations/6736a10b303d062c7fe69f0a.tf
@@ -12,7 +12,14 @@ resource "segment_destination" "id-6736a10b303d062c7fe69f0a" {
     region_endpoints  = ["US", "EU"]
     supported_regions = ["us-west-2", "eu-west-1"]
   }
-  name      = "Braze Cloud Mode (Actions)"
-  settings  = "{\"api_key\":\"••••••••••8220\",\"app_id\":\"\",\"endpoint\":\"https://rest.iad-01.braze.com\"}"
+  name = "Braze Cloud Mode (Actions)"
+  settings = jsonencode({
+    api_key = "••••••••••a387"
+    app_id  = ""
+    dynamicAuthSettings = {
+      configId = "6736a10b303d062c7fe69f0a"
+    }
+    endpoint = "https://rest.iad-01.braze.com"
+  })
   source_id = "7U1pz1W1ESiasVbLSGWWzT"
 }
