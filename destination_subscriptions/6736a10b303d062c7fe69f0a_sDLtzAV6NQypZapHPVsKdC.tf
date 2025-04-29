4d8a4c8ea3df58fb9c2e7d256b66770627a0da2e
Segment Git Sync <friends@segment.com>
2025-04-29 01:04:20 +0000

    Destination Subscription 'sDLtzAV6NQypZapHPVsKdC' modified by cortega@twilio.com

--- destination_subscriptions/6736a10b303d062c7fe69f0a_sDLtzAV6NQypZapHPVsKdC.tf
+++ destination_subscriptions/6736a10b303d062c7fe69f0a_sDLtzAV6NQypZapHPVsKdC.tf
@@ -6,54 +6,54 @@ import {
 resource "segment_destination_subscription" "id-6736a10b303d062c7fe69f0a_sDLtzAV6NQypZapHPVsKdC" {
   action_id            = "2P24zUSAL8BUpyGYNGmD7M"
   destination_id       = "6736a10b303d062c7fe69f0a"
-  enabled              = false
+  enabled              = true
   model_id             = null
-  name                 = "Identify Calls"
+  name                 = "Engage - Identify Calls"
   reverse_etl_schedule = null
   settings = jsonencode({
     _update_existing_only = false
     batch_size            = 75
     braze_id = {
-      "@template" = "{{properties.braze_id}}"
+      "@path" = "$.properties.braze_id"
     }
     country = {
-      "@template" = "{{context.location.country}}"
+      "@path" = "$.context.location.country"
     }
     current_location = {
       latitude = {
-        "@template" = "{{context.location.latitude}}"
+        "@path" = "$.context.location.latitude"
       }
       longitude = {
-        "@template" = "{{context.location.longitude}}"
+        "@path" = "$.context.location.longitude"
       }
     }
     custom_attributes = {
-      "@template" = "{{traits}}"
+      "@path" = "$.traits"
     }
     email = {
-      "@template" = "{{traits.email}}"
+      "@path" = "$.traits.email"
     }
     enable_batching = true
     external_id = {
-      "@template" = "{{userId}}"
+      "@path" = "$.userId"
     }
     first_name = {
-      "@template" = "{{traits.firstName}}"
+      "@path" = "$.traits.firstName"
     }
     gender = {
-      "@template" = "{{traits.gender}}"
+      "@path" = "$.traits.gender"
     }
     home_city = {
-      "@template" = "{{traits.address.city}}"
+      "@path" = "$.traits.address.city"
     }
     image_url = {
-      "@template" = "{{traits.avatar}}"
+      "@path" = "$.traits.avatar"
     }
     last_name = {
-      "@template" = "{{traits.lastName}}"
+      "@path" = "$.traits.lastName"
     }
     phone = {
-      "@template" = "{{traits.phone}}"
+      "@path" = "$.traits.phone"
     }
   })
   trigger = "type = \"identify\""
