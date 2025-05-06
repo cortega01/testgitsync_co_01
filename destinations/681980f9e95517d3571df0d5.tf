5da5ed8699cccfe096553977f89f7d9c7acab7ec
Segment Git Sync <friends@segment.com>
2025-05-06 03:24:43 +0000

    Destination '681980f9e95517d3571df0d5' created by cortega@twilio.com

---
+++ destinations/681980f9e95517d3571df0d5.tf
@@ -0,0 +1,20 @@
+import {
+  to = segment_destination.id-681980f9e95517d3571df0d5
+  id = "681980f9e95517d3571df0d5"
+}
+
+resource "segment_destination" "id-681980f9e95517d3571df0d5" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "614a3c7d791c91c41bae7599"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "Webhook Actions Test"
+  settings = jsonencode({
+    sharedSecret = ""
+  })
+  source_id = "oJPmNYZu1jHNgmgk6q2iuf"
+}
