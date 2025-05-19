2d39e1207cf2511704620e2dde96007ed53612f6
Segment Git Sync <friends@segment.com>
2025-05-19 23:07:59 +0000

    Destination Subscription 'aSvn2DnhC76xovqXSVZXGm' disabled by internal-service@segment.com

--- destination_subscriptions/6743ee6845bb606d1c4c51f9_aSvn2DnhC76xovqXSVZXGm.tf
+++ destination_subscriptions/6743ee6845bb606d1c4c51f9_aSvn2DnhC76xovqXSVZXGm.tf
@@ -4,11 +4,29 @@ import {
 }
 
 resource "segment_destination_subscription" "id-6743ee6845bb606d1c4c51f9_aSvn2DnhC76xovqXSVZXGm" {
-  action_id      = "5YPbdm7ah3qQRMGnQDrYAy"
-  destination_id = "6743ee6845bb606d1c4c51f9"
-  enabled        = true
-  model_id       = null
-  name           = "Order Completed"
-  settings       = "{\"event_name\":\"Order Completed\",\"profile\":{\"email\":{\"@path\":\"$.context.traits.email\"}},\"properties\":{\"@path\":\"$.properties\"},\"unique_id\":{\"@path\":\"$.messageId\"}}"
-  trigger        = "type = \"track\""
+  action_id            = "5YPbdm7ah3qQRMGnQDrYAy"
+  destination_id       = "6743ee6845bb606d1c4c51f9"
+  enabled              = true
+  model_id             = null
+  name                 = "Order Completed"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    event_name         = "Order Completed"
+    product_event_name = "Ordered Product"
+    profile = {
+      email = {
+        "@path" = "$.context.traits.email"
+      }
+    }
+    properties = {
+      "@path" = "$.properties"
+    }
+    time = {
+      "@path" = "$.timestamp"
+    }
+    unique_id = {
+      "@path" = "$.messageId"
+    }
+  })
+  trigger = "type = \"track\""
 }
