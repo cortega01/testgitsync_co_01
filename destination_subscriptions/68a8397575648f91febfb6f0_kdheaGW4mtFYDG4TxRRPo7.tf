bf16a621bdf5360d566c22b00b8221f39fc2ae80
Segment Git Sync <friends@segment.com>
2025-08-22 10:30:25 +0000

    Destination Subscription 'kdheaGW4mtFYDG4TxRRPo7' created by cortega@twilio.com

---
+++ destination_subscriptions/68a8397575648f91febfb6f0_kdheaGW4mtFYDG4TxRRPo7.tf
@@ -0,0 +1,32 @@
+import {
+  to = segment_destination_subscription.id-68a8397575648f91febfb6f0_kdheaGW4mtFYDG4TxRRPo7
+  id = "68a8397575648f91febfb6f0:kdheaGW4mtFYDG4TxRRPo7"
+}
+
+resource "segment_destination_subscription" "id-68a8397575648f91febfb6f0_kdheaGW4mtFYDG4TxRRPo7" {
+  action_id      = "oiFvX4YXwyaPVysi1t7mtz"
+  destination_id = "68a8397575648f91febfb6f0"
+  enabled        = false
+  model_id       = "99ajX6rZiHagThqHiS8z62"
+  name           = "Send Track (Copy)"
+  reverse_etl_schedule = {
+    config   = null
+    strategy = "MANUAL"
+  }
+  settings = jsonencode({
+    consent = {
+      "@template" = "{{context.consent}}"
+    }
+    event_name = "rETL Test Added or Updated"
+    message_id = {
+      "@template" = "{{messageId}}"
+    }
+    properties = {
+      propertyNum = 1
+    }
+    user_id = {
+      "@template" = "{{properties.CONTACT_ID}}"
+    }
+  })
+  trigger = "event = \"new\" or event = \"updated\""
+}
