c74ec91c84a76731c2a24657986ad025038d52bb
Segment Git Sync <friends@segment.com>
2025-08-22 10:05:52 +0000

    Destination Subscription 'bkdbw5eGeZ5J2W4YiLnkc1' created by cortega@twilio.com

---
+++ destination_subscriptions/68a8397575648f91febfb6f0_bkdbw5eGeZ5J2W4YiLnkc1.tf
@@ -0,0 +1,32 @@
+import {
+  to = segment_destination_subscription.id-68a8397575648f91febfb6f0_bkdbw5eGeZ5J2W4YiLnkc1
+  id = "68a8397575648f91febfb6f0:bkdbw5eGeZ5J2W4YiLnkc1"
+}
+
+resource "segment_destination_subscription" "id-68a8397575648f91febfb6f0_bkdbw5eGeZ5J2W4YiLnkc1" {
+  action_id      = "oiFvX4YXwyaPVysi1t7mtz"
+  destination_id = "68a8397575648f91febfb6f0"
+  enabled        = false
+  model_id       = "99ajX6rZiHagThqHiS8z62"
+  name           = "Send Track (Copy)"
+  reverse_etl_schedule = {
+    config   = null
+    strategy = "MANUAL"
+  }
+  settings = jsonencode({
+    consent = {
+      "@template" = "{{context.consent}}"
+    }
+    event_name = "rETL Test Added or Updated"
+    message_id = {
+      "@template" = "{{messageId}}"
+    }
+    properties = {
+      propertyNum = 1
+    }
+    user_id = {
+      "@template" = "{{properties.CONTACT_ID}}"
+    }
+  })
+  trigger = "event = \"new\" or event = \"updated\""
+}
