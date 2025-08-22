da540b113b2fa49407ef986f24e0f24cdc9eb590
Segment Git Sync <friends@segment.com>
2025-08-22 10:08:56 +0000

    Destination Subscription 'tMxeCYHiNTMcGYezAsmnJH' modified by cortega@twilio.com

--- destination_subscriptions/68a8397575648f91febfb6f0_tMxeCYHiNTMcGYezAsmnJH.tf
+++ destination_subscriptions/68a8397575648f91febfb6f0_tMxeCYHiNTMcGYezAsmnJH.tf
@@ -8,24 +8,24 @@ resource "segment_destination_subscription" "id-68a8397575648f91febfb6f0_tMxeCYH
   destination_id = "68a8397575648f91febfb6f0"
   enabled        = false
   model_id       = "99ajX6rZiHagThqHiS8z62"
-  name           = "Send Track (Copy)"
+  name           = "Send Track (DELETE)"
   reverse_etl_schedule = {
     config   = null
     strategy = "MANUAL"
   }
   settings = jsonencode({
     consent = {
-      "@template" = "{{context.consent}}"
+      "@path" = "$.context.consent"
     }
-    event_name = "rETL Test Added or Updated"
+    event_name = "rETL Test DELETED"
     message_id = {
-      "@template" = "{{messageId}}"
+      "@path" = "$.messageId"
     }
     properties = {
       propertyNum = 1
     }
     user_id = {
-      "@template" = "{{properties.CONTACT_ID}}"
+      "@path" = "$.properties.CONTACT_ID"
     }
   })
   trigger = "event = \"new\" or event = \"updated\""
