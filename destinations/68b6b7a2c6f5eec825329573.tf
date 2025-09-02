93aacfc855e9d21848a99de83585d0b510188f0f
Segment Git Sync <friends@segment.com>
2025-09-02 09:23:48 +0000

    Destination '68b6b7a2c6f5eec825329573' created by cortega@twilio.com

---
+++ destinations/68b6b7a2c6f5eec825329573.tf
@@ -0,0 +1,24 @@
+import {
+  to = segment_destination.id-68b6b7a2c6f5eec825329573
+  id = "68b6b7a2c6f5eec825329573"
+}
+
+resource "segment_destination" "id-68b6b7a2c6f5eec825329573" {
+  enabled = false
+  metadata = {
+    contacts = [
+      {
+      },
+    ]
+    id                = "60ae8b97dcb6cc52d5d0d5ab"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "Google Ads Conversions"
+  settings = jsonencode({
+    conversionTrackingId = ""
+    customerId           = ""
+  })
+  source_id = "7U1pz1W1ESiasVbLSGWWzT"
+}
