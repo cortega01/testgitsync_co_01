98fa77550467ae22eaee2f5dd50b5fa73708a167
Segment Git Sync <friends@segment.com>
2025-09-08 02:19:21 +0000

    Source '2GkKP8PgeYPHAr2kCkRTLz' created by cortega@twilio.com

---
+++ sources/2GkKP8PgeYPHAr2kCkRTLz.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-2GkKP8PgeYPHAr2kCkRTLz
+  id = "2GkKP8PgeYPHAr2kCkRTLz"
+}
+
+resource "segment_source" "id-2GkKP8PgeYPHAr2kCkRTLz" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "pHcia14h1B"
+  }
+  name     = "Amplitude Cohorts"
+  settings = jsonencode({})
+  slug     = "amplitude_cohorts"
+}
