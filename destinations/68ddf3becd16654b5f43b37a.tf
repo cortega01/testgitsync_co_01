ddf9029c09ea5fdc77efa4b45396cbe2299e2c35
Segment Git Sync <friends@segment.com>
2025-10-02 03:38:39 +0000

    Destination '68ddf3becd16654b5f43b37a' created by cortega@twilio.com

---
+++ destinations/68ddf3becd16654b5f43b37a.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_destination.id-68ddf3becd16654b5f43b37a
+  id = "68ddf3becd16654b5f43b37a"
+}
+
+resource "segment_destination" "id-68ddf3becd16654b5f43b37a" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "60f9d0d048950c356be2e4da"
+    partner_owned     = false
+    region_endpoints  = ["US", "EU"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "Braze Cloud Mode (Actions) (2)"
+  settings = jsonencode({
+    api_key  = ""
+    app_id   = ""
+    endpoint = "https://rest.iad-01.braze.com"
+  })
+  source_id = "d6bAafTDmcvXKL5fyzb5jg"
+}
