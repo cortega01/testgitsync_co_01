f067074dd4e3508f1248f734e876de44d022da63
Segment Git Sync <friends@segment.com>
2025-12-05 01:30:27 +0000

    Destination Filter '36PBFysd0yUZUQGpevcNnGahKxb' created by Public API token 'Public API Test 1'

---
+++ destination_filters/36PBFysd0yUZUQGpevcNnGahKxb.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_destination_filter.id-69322a7307f444425e48ddb1_36PBFysd0yUZUQGpevcNnGahKxb
+  id = "69322a7307f444425e48ddb1:36PBFysd0yUZUQGpevcNnGahKxb"
+}
+
+resource "segment_destination_filter" "id-69322a7307f444425e48ddb1_36PBFysd0yUZUQGpevcNnGahKxb" {
+  actions = [
+    {
+      fields  = null
+      path    = null
+      percent = null
+      type    = "DROP"
+    },
+  ]
+  description    = "Only send event if context.campaign.source, medium, or name is not null"
+  destination_id = "69322a7307f444425e48ddb1"
+  enabled        = true
+  if             = "(context.campaign.source = null or length( context.campaign.source ) = 0) and (context.campaign.medium = null or length( context.campaign.medium ) = 0) and (context.campaign.medium = null or length( context.campaign.name ) = 0)"
+  source_id      = "FBYFyj8QNqAnWaULMEx6e"
+  title          = "Any event type with campaign clicks"
+}
