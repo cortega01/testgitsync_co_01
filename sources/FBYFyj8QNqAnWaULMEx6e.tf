bb9be816103c2dc5dd5a228dbcc6354ee8963175
Segment Git Sync <friends@segment.com>
2025-12-05 00:40:39 +0000

    Source 'FBYFyj8QNqAnWaULMEx6e' created by cortega@twilio.com

---
+++ sources/FBYFyj8QNqAnWaULMEx6e.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-FBYFyj8QNqAnWaULMEx6e
+  id = "FBYFyj8QNqAnWaULMEx6e"
+}
+
+resource "segment_source" "id-FBYFyj8QNqAnWaULMEx6e" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "iUM16Md8P2"
+  }
+  name     = "Hygiene Source"
+  settings = jsonencode({})
+  slug     = "hygiene_source"
+}
