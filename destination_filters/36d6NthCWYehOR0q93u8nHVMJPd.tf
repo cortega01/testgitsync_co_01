76843b7f950fe561bdb5d8c423ee40b63b7d2865
Segment Git Sync <friends@segment.com>
2025-12-09 23:47:46 +0000

    Destination Filter '36d6NthCWYehOR0q93u8nHVMJPd' created by Public API token 'Public API Test 1'

---
+++ destination_filters/36d6NthCWYehOR0q93u8nHVMJPd.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_destination_filter.id-69322a7307f444425e48ddb1_36d6NthCWYehOR0q93u8nHVMJPd
+  id = "69322a7307f444425e48ddb1:36d6NthCWYehOR0q93u8nHVMJPd"
+}
+
+resource "segment_destination_filter" "id-69322a7307f444425e48ddb1_36d6NthCWYehOR0q93u8nHVMJPd" {
+  actions = [
+    {
+      fields  = null
+      path    = null
+      percent = null
+      type    = "DROP"
+    },
+  ]
+  description    = "Drop events if they are not in specified in the query"
+  destination_id = "69322a7307f444425e48ddb1"
+  enabled        = true
+  if             = "type = 'track' and !(event = 'Event A' or event = 'Event B')"
+  source_id      = "FBYFyj8QNqAnWaULMEx6e"
+  title          = "Track Event Allowlist"
+}
