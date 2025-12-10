9e818d2e6458578c0a6cd279aae9b6f9ec0b5c52
Segment Git Sync <friends@segment.com>
2025-12-10 00:03:35 +0000

    Destination Filter '36d6NthCWYehOR0q93u8nHVMJPd' modified by Public API token 'Public API Test 1'

--- destination_filters/36d6NthCWYehOR0q93u8nHVMJPd.tf
+++ destination_filters/36d6NthCWYehOR0q93u8nHVMJPd.tf
@@ -15,7 +15,7 @@ resource "segment_destination_filter" "id-69322a7307f444425e48ddb1_36d6NthCWYehO
   description    = "Drop events if they are not in specified in the query"
   destination_id = "69322a7307f444425e48ddb1"
   enabled        = true
-  if             = "type = 'track' and !(event = 'Event A' or event = 'Event B')"
+  if             = "type = 'track' and !(event in ['Event A', 'Event B'])"
   source_id      = "FBYFyj8QNqAnWaULMEx6e"
   title          = "Track Event Allowlist"
 }
