4ed8a4cfe4bde1915e9a7480b6c19ab40ec99f93
Segment Git Sync <friends@segment.com>
2026-01-21 04:46:44 +0000

    Source '68Ymf3CBXBa5YC2kTKzkNs' modified by cortega@twilio.com

--- sources/68Ymf3CBXBa5YC2kTKzkNs.tf
+++ sources/68Ymf3CBXBa5YC2kTKzkNs.tf
@@ -9,7 +9,12 @@ resource "segment_source" "id-68Ymf3CBXBa5YC2kTKzkNs" {
   metadata = {
     id = "BQp3zGUgjV"
   }
-  name     = "Entity Group: snowflake in spa_2JAiSpeDFhNt1oMXyNMXJQ"
-  settings = "{\"account\":\"ecb63470.prod3.us-west-2\",\"database\":\"LINKED_WORKSHOP\",\"username\":\"LINKED\",\"warehouse\":\"SEGMENT_WAREHOUSE\"}"
-  slug     = "entity-group-snowflake-spa_2jaispedfhnt1omxynmxjq"
+  name = "Entity Group: snowflake in spa_2JAiSpeDFhNt1oMXyNMXJQ"
+  settings = jsonencode({
+    account   = "ZS53153-SEGMENT_PARTNER"
+    database  = "CORTEGA_SANDBOX"
+    username  = "CORTEGA"
+    warehouse = "SEGMENT_WAREHOUSE"
+  })
+  slug = "entity-group-snowflake-spa_2jaispedfhnt1omxynmxjq"
 }
