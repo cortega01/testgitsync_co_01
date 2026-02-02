af589816122351cceb5537c00546050824c5f928
Segment Git Sync <friends@segment.com>
2026-02-02 04:11:27 +0000

    Destination '681980f9e95517d3571df0d5' modified by cortega@twilio.com

--- destinations/681980f9e95517d3571df0d5.tf
+++ destinations/681980f9e95517d3571df0d5.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-681980f9e95517d3571df0d5" {
-  enabled = true
+  enabled = false
   metadata = {
     contacts          = null
     id                = "614a3c7d791c91c41bae7599"
@@ -16,6 +16,9 @@ resource "segment_destination" "id-681980f9e95517d3571df0d5" {
   settings = jsonencode({
     dynamicAuthSettings = {
       configId = "681980f9e95517d3571df0d5"
+      oauth = {
+        type = "noAuth"
+      }
     }
     sharedSecret = ""
   })
