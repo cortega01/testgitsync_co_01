79d6a24154f6d25f30350151ea765787a674854b
Segment Git Sync <friends@segment.com>
2026-02-12 05:14:49 +0000

    Destination '698d61c7eb560600d273da75' created by cortega@twilio.com

---
+++ destinations/698d61c7eb560600d273da75.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_destination.id-698d61c7eb560600d273da75
+  id = "698d61c7eb560600d273da75"
+}
+
+resource "segment_destination" "id-698d61c7eb560600d273da75" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "698d614eafbac93890d8d27c"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name = "Javascript Dev to sGTM container #1"
+  settings = jsonencode({
+    apiSecret     = ""
+    measurementId = ""
+  })
+  source_id = "f9XSryLD3S4oTyquhekAit"
+}
