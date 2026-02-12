c052a5757395ba6a095e80aead79b69142fa7f55
Segment Git Sync <friends@segment.com>
2026-02-12 03:08:11 +0000

    Destination '65cec19f8493a8ff8ef0eac3' modified by cortega@twilio.com

--- destinations/65cec19f8493a8ff8ef0eac3.tf
+++ destinations/65cec19f8493a8ff8ef0eac3.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-65cec19f8493a8ff8ef0eac3" {
-  enabled = true
+  enabled = false
   metadata = {
     contacts = [
       {
@@ -15,7 +15,17 @@ resource "segment_destination" "id-65cec19f8493a8ff8ef0eac3" {
     region_endpoints  = ["EU", "US"]
     supported_regions = ["us-west-2", "eu-west-1"]
   }
-  name      = "MoEngage Instance #1"
-  settings  = "{\"api_id\":\"KM5578PTG12EDAZYE5U1BMXK\",\"api_key\":\"VF8joUKEQ3PO@4sP2@eyyZ8R\",\"region\":\"DC_01\"}"
+  name = "MoEngage Instance #1"
+  settings = jsonencode({
+    api_id  = "KM5578PTG12EDAZYE5U1BMXK"
+    api_key = "••••••••••yZ8R"
+    dynamicAuthSettings = {
+      configId = "65cec19f8493a8ff8ef0eac3"
+      oauth = {
+        type = "noAuth"
+      }
+    }
+    region = "DC_01"
+  })
   source_id = "f9XSryLD3S4oTyquhekAit"
 }
