ada6839e2515d02bf6f11d3be78106f24075bdd8
Segment Git Sync <friends@segment.com>
2026-02-12 03:07:31 +0000

    Destination '6414c27fc54cff1c3d39c30c' modified by cortega@twilio.com

--- destinations/6414c27fc54cff1c3d39c30c.tf
+++ destinations/6414c27fc54cff1c3d39c30c.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-6414c27fc54cff1c3d39c30c" {
-  enabled = true
+  enabled = false
   metadata = {
     contacts          = null
     id                = "614a3c7d791c91c41bae7599"
@@ -12,7 +12,15 @@ resource "segment_destination" "id-6414c27fc54cff1c3d39c30c" {
     region_endpoints  = ["US"]
     supported_regions = ["us-west-2", "eu-west-1"]
   }
-  name      = "Webhook (Actions) Test 1"
-  settings  = "{\"sharedSecret\":\"\"}"
+  name = "Webhook (Actions) Test 1"
+  settings = jsonencode({
+    dynamicAuthSettings = {
+      configId = "6414c27fc54cff1c3d39c30c"
+      oauth = {
+        type = "noAuth"
+      }
+    }
+    sharedSecret = ""
+  })
   source_id = "f9XSryLD3S4oTyquhekAit"
 }
