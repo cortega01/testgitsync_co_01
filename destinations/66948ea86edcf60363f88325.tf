fed912af796fc4aa0d81cad2ecd28e9a9d6a38d3
Segment Git Sync <friends@segment.com>
2026-02-12 03:06:28 +0000

    Destination '66948ea86edcf60363f88325' modified by cortega@twilio.com

--- destinations/66948ea86edcf60363f88325.tf
+++ destinations/66948ea86edcf60363f88325.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-66948ea86edcf60363f88325" {
-  enabled = true
+  enabled = false
   metadata = {
     contacts          = null
     id                = "5f7dd6d21ad74f3842b1fc47"
@@ -12,7 +12,17 @@ resource "segment_destination" "id-66948ea86edcf60363f88325" {
     region_endpoints  = ["US", "EU"]
     supported_regions = ["us-west-2", "eu-west-1"]
   }
-  name      = "Amplitude Actions Web"
-  settings  = "{\"apiKey\":\"••••••••••57e4\",\"endpoint\":\"north_america\",\"secretKey\":\"••••••••••c1e3\"}"
+  name = "Amplitude Actions Web"
+  settings = jsonencode({
+    apiKey = "••••••••••57e4"
+    dynamicAuthSettings = {
+      configId = "66948ea86edcf60363f88325"
+      oauth = {
+        type = "noAuth"
+      }
+    }
+    endpoint  = "north_america"
+    secretKey = "••••••••••c1e3"
+  })
   source_id = "f9XSryLD3S4oTyquhekAit"
 }
