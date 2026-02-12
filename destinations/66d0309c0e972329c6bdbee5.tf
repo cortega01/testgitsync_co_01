95c2e1b98e923c49321bf76c4ec7cda28b06c641
Segment Git Sync <friends@segment.com>
2026-02-12 03:06:40 +0000

    Destination '66d0309c0e972329c6bdbee5' disabled by cortega@twilio.com

--- destinations/66d0309c0e972329c6bdbee5.tf
+++ destinations/66d0309c0e972329c6bdbee5.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-66d0309c0e972329c6bdbee5" {
-  enabled = true
+  enabled = false
   metadata = {
     contacts          = null
     id                = "54521fd525e721e32a72eeac"
@@ -12,7 +12,21 @@ resource "segment_destination" "id-66d0309c0e972329c6bdbee5" {
     region_endpoints  = null
     supported_regions = null
   }
-  name      = "Eloqua Destination Test JS"
-  settings  = "{\"companyName\":\"TechnologyPartnerSegmentio\",\"createContactOnTrack\":false,\"mappedEvents\":{\"Button Clicked\":\"Button Clicked\"},\"mappedGroupTraits\":{},\"mappedIdentifyTraits\":{\"contactType\":\"contactType\"},\"password\":\"••••••••••234!\",\"sendGroup\":false,\"siteId\":\"p02\",\"username\":\"Calvin.French-Owen\"}"
+  name = "Eloqua Destination Test JS"
+  settings = jsonencode({
+    companyName          = "TechnologyPartnerSegmentio"
+    createContactOnTrack = false
+    mappedEvents = {
+      "Button Clicked" = "Button Clicked"
+    }
+    mappedGroupTraits = {}
+    mappedIdentifyTraits = {
+      contactType = "contactType"
+    }
+    password  = "••••••••••234!"
+    sendGroup = false
+    siteId    = "p02"
+    username  = "Calvin.French-Owen"
+  })
   source_id = "f9XSryLD3S4oTyquhekAit"
 }
