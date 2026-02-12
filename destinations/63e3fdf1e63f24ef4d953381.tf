17954b85afa6cfa263a8e9ca122cb72e35b16a76
Segment Git Sync <friends@segment.com>
2026-02-12 03:07:55 +0000

    Destination '63e3fdf1e63f24ef4d953381' disabled by cortega@twilio.com

--- destinations/63e3fdf1e63f24ef4d953381.tf
+++ destinations/63e3fdf1e63f24ef4d953381.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-63e3fdf1e63f24ef4d953381" {
-  enabled = true
+  enabled = false
   metadata = {
     contacts          = null
     id                = "5f7dd8e302173ff732db5cc4"
@@ -12,7 +12,14 @@ resource "segment_destination" "id-63e3fdf1e63f24ef4d953381" {
     region_endpoints  = ["US"]
     supported_regions = ["eu-west-1", "us-west-2"]
   }
-  name      = "Slack Test 1"
-  settings  = "{}"
+  name = "Slack Test 1"
+  settings = jsonencode({
+    dynamicAuthSettings = {
+      configId = "63e3fdf1e63f24ef4d953381"
+      oauth = {
+        type = "noAuth"
+      }
+    }
+  })
   source_id = "f9XSryLD3S4oTyquhekAit"
 }
