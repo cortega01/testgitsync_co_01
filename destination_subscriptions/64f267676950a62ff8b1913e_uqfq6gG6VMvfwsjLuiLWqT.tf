c0bc9e5c1c885375e9ef8275276c93eed66cf080
Segment Git Sync <friends@segment.com>
2026-02-17 02:29:00 +0000

    Destination Subscription 'uqfq6gG6VMvfwsjLuiLWqT' modified by cortega@twilio.com

--- destination_subscriptions/64f267676950a62ff8b1913e_uqfq6gG6VMvfwsjLuiLWqT.tf
+++ destination_subscriptions/64f267676950a62ff8b1913e_uqfq6gG6VMvfwsjLuiLWqT.tf
@@ -4,11 +4,20 @@ import {
 }
 
 resource "segment_destination_subscription" "id-64f267676950a62ff8b1913e_uqfq6gG6VMvfwsjLuiLWqT" {
-  action_id      = "hhcbf2MTRHEXqkz9AipW4V"
-  destination_id = "64f267676950a62ff8b1913e"
-  enabled        = true
-  model_id       = null
-  name           = "Set Configuration Fields"
-  settings       = "{\"campaign_term\":{\"@path\":\"$.context.campaign.term\"},\"send_page_view\":true,\"user_id\":{\"@path\":\"$.userId\"}}"
-  trigger        = "type = \"page\" or type = \"identify\""
+  action_id            = "hhcbf2MTRHEXqkz9AipW4V"
+  destination_id       = "64f267676950a62ff8b1913e"
+  enabled              = false
+  model_id             = null
+  name                 = "Set Configuration Fields"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    campaign_term = {
+      "@path" = "$.context.campaign.term"
+    }
+    send_page_view = true
+    user_id = {
+      "@path" = "$.userId"
+    }
+  })
+  trigger = "type = \"page\" or type = \"identify\""
 }
