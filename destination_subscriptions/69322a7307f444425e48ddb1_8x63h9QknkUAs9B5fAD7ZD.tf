728cd44b2869697181c37d0b1be2c871774612ea
Segment Git Sync <friends@segment.com>
2026-03-05 01:37:12 +0000

    Destination Subscription '8x63h9QknkUAs9B5fAD7ZD' modified by cortega@twilio.com

--- destination_subscriptions/69322a7307f444425e48ddb1_8x63h9QknkUAs9B5fAD7ZD.tf
+++ destination_subscriptions/69322a7307f444425e48ddb1_8x63h9QknkUAs9B5fAD7ZD.tf
@@ -15,8 +15,12 @@ resource "segment_destination_subscription" "id-69322a7307f444425e48ddb1_8x63h9Q
     data = {
       "@path" = "$."
     }
+    headers = {
+      Authorization = "Basic a3B1ZGtkRXB6Ykc2TlBBQmNJTE9Ta3l6Tmoxa2FIQ0o6"
+      Content-Type  = "application/json"
+    }
     method = "POST"
-    url    = "https://webhook.site/81aee949-0dfc-4910-9ffb-885359c91bd4"
+    url    = "https://api.segment.io/v1/track"
   })
-  trigger = "type = \"track\" or type = \"identify\""
+  trigger = "type = \"track\" or contains(context.app.namespace, \".og.\")"
 }
