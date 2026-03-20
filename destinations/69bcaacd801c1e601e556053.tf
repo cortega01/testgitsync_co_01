94068e74c1ef8888f8628cf316c95c312bb3881f
Segment Git Sync <friends@segment.com>
2026-03-20 02:02:54 +0000

    Destination '69bcaacd801c1e601e556053' created by cortega@twilio.com

---
+++ destinations/69bcaacd801c1e601e556053.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-69bcaacd801c1e601e556053
+  id = "69bcaacd801c1e601e556053"
+}
+
+resource "segment_destination" "id-69bcaacd801c1e601e556053" {
+  enabled = true
+  metadata = {
+    contacts          = null
+    id                = "639c2dbb1309fdcad13951b6"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "rETL Snowflake JPANG CORTEGA SANDBOX Segment Profiles"
+  settings  = jsonencode({})
+  source_id = "jAW8V8J8zbXFEVXhvjCmGn"
+}
