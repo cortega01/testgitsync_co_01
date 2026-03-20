64ada133ccdc6f9de2161fc07096afb4d91fcca0
Segment Git Sync <friends@segment.com>
2026-03-20 05:21:38 +0000

    Destination Subscription 'cVRUQgKMpjgPqvVvAq2GBX' created by cortega@twilio.com

---
+++ destination_subscriptions/69bcaacd801c1e601e556053_cVRUQgKMpjgPqvVvAq2GBX.tf
@@ -0,0 +1,36 @@
+import {
+  to = segment_destination_subscription.id-69bcaacd801c1e601e556053_cVRUQgKMpjgPqvVvAq2GBX
+  id = "69bcaacd801c1e601e556053:cVRUQgKMpjgPqvVvAq2GBX"
+}
+
+resource "segment_destination_subscription" "id-69bcaacd801c1e601e556053_cVRUQgKMpjgPqvVvAq2GBX" {
+  action_id      = "vm2tNA5U1zxUusQkdC4D7Z"
+  destination_id = "69bcaacd801c1e601e556053"
+  enabled        = false
+  model_id       = "qohw1kDtjXB6tPn4PmrD27"
+  name           = "Send Identify"
+  reverse_etl_schedule = {
+    config = jsonencode({
+      interval = "1h"
+    })
+    strategy = "PERIODIC"
+  }
+  settings = jsonencode({
+    consent = {
+      "@path" = "$.context.consent"
+    }
+    engage_space = "wDPRhb6pG0A0gCUtmmXkw3rZVdgQZsrW"
+    message_id = {
+      "@path" = "$.messageId"
+    }
+    traits = {
+      primary_id = {
+        "@path" = "$.properties.PRIMARY_ID"
+      }
+    }
+    user_id = {
+      "@path" = "$.properties.UID"
+    }
+  })
+  trigger = "event = \"new\" or event = \"updated\""
+}
