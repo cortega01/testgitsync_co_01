5ea56ade82d466cbdcc7a9470899efd97d9d3a95
Segment Git Sync <friends@segment.com>
2026-05-12 00:50:56 +0000

    Source '68Ymf3CBXBa5YC2kTKzkNs' modified by cortega@twilio.com

--- sources/68Ymf3CBXBa5YC2kTKzkNs.tf
+++ sources/68Ymf3CBXBa5YC2kTKzkNs.tf
@@ -12,8 +12,8 @@ resource "segment_source" "id-68Ymf3CBXBa5YC2kTKzkNs" {
   name = "Entity Group: snowflake in spa_2JAiSpeDFhNt1oMXyNMXJQ"
   settings = jsonencode({
     account   = "ZS53153-SEGMENT_PARTNER"
-    database  = "CORTEGA_SANDBOX"
-    username  = "CORTEGA"
+    database  = "JPANG_SANDBOX"
+    username  = "JEREMY_SA"
     warehouse = "SEGMENT_WAREHOUSE"
   })
   slug = "entity-group-snowflake-spa_2jaispedfhnt1omxynmxjq"
