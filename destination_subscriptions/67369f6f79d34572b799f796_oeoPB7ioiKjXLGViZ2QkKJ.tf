0effb8ce41c5d6eabd895e28553e894f9f6422cf
Segment Git Sync <friends@segment.com>
2026-05-12 00:57:00 +0000

    Destination Subscription 'oeoPB7ioiKjXLGViZ2QkKJ' modified by cortega@twilio.com

--- destination_subscriptions/67369f6f79d34572b799f796_oeoPB7ioiKjXLGViZ2QkKJ.tf
+++ destination_subscriptions/67369f6f79d34572b799f796_oeoPB7ioiKjXLGViZ2QkKJ.tf
@@ -4,11 +4,36 @@ import {
 }
 
 resource "segment_destination_subscription" "id-67369f6f79d34572b799f796_oeoPB7ioiKjXLGViZ2QkKJ" {
-  action_id      = "vE7Gf9yobj2gTuMBhwmg7g"
-  destination_id = "67369f6f79d34572b799f796"
-  enabled        = true
-  model_id       = null
-  name           = "Order Completed Calls"
-  settings       = "{\"_update_existing_only\":false,\"batch_size\":75,\"braze_id\":{\"@path\":\"$.properties.braze_id\"},\"email\":{\"@path\":\"$.traits.email\"},\"enable_batching\":false,\"external_id\":{\"@path\":\"$.userId\"},\"products\":{\"@arrayPath\":[{\"@path\":\"$.properties.products\"},null]},\"properties\":{\"@path\":\"$.properties\"},\"time\":{\"@path\":\"$.receivedAt\"}}"
-  trigger        = "event = \"Order Completed\""
+  action_id            = "vE7Gf9yobj2gTuMBhwmg7g"
+  destination_id       = "67369f6f79d34572b799f796"
+  enabled              = false
+  model_id             = null
+  name                 = "Order Completed Calls"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    _update_existing_only = false
+    batch_size            = 75
+    braze_id = {
+      "@path" = "$.properties.braze_id"
+    }
+    email = {
+      "@path" = "$.traits.email"
+    }
+    enable_batching = false
+    external_id = {
+      "@path" = "$.userId"
+    }
+    products = {
+      "@arrayPath" = [{
+        "@path" = "$.properties.products"
+      }, null]
+    }
+    properties = {
+      "@path" = "$.properties"
+    }
+    time = {
+      "@path" = "$.receivedAt"
+    }
+  })
+  trigger = "event = \"Order Completed\""
 }
