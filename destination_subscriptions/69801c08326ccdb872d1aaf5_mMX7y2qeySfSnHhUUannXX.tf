d962667566d4c3c3e6678efe9056d2b447e4ecb9
Segment Git Sync <friends@segment.com>
2026-05-26 06:07:12 +0000

    Destination Subscription 'mMX7y2qeySfSnHhUUannXX' created by cortega@twilio.com

---
+++ destination_subscriptions/69801c08326ccdb872d1aaf5_mMX7y2qeySfSnHhUUannXX.tf
@@ -0,0 +1,23 @@
+import {
+  to = segment_destination_subscription.id-69801c08326ccdb872d1aaf5_mMX7y2qeySfSnHhUUannXX
+  id = "69801c08326ccdb872d1aaf5:mMX7y2qeySfSnHhUUannXX"
+}
+
+resource "segment_destination_subscription" "id-69801c08326ccdb872d1aaf5_mMX7y2qeySfSnHhUUannXX" {
+  action_id            = "nFPnRozhz1mh4Gbx4MLvT5"
+  destination_id       = "69801c08326ccdb872d1aaf5"
+  enabled              = false
+  model_id             = null
+  name                 = "Send"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    batch_keys = ["url", "method", "headers"]
+    data = {
+      "@path" = "$."
+    }
+    enable_batching = false
+    method          = "POST"
+    url             = "https://webhook.site/118f2024-9928-4b9e-a613-dc94171b9258"
+  })
+  trigger = "event = \"Test Event\""
+}
