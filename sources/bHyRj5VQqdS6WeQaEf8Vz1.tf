97d1cd71570c492251af6a405aaa5a39cd3d2840
Segment Git Sync <friends@segment.com>
2026-05-26 00:49:39 +0000

    Source 'bHyRj5VQqdS6WeQaEf8Vz1' created by cortega@twilio.com

---
+++ sources/bHyRj5VQqdS6WeQaEf8Vz1.tf
@@ -0,0 +1,17 @@
+import {
+  to = segment_source.id-bHyRj5VQqdS6WeQaEf8Vz1
+  id = "bHyRj5VQqdS6WeQaEf8Vz1"
+}
+
+resource "segment_source" "id-bHyRj5VQqdS6WeQaEf8Vz1" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "9EMcTqiKok"
+  }
+  name = "Kotlin (Android) Dev"
+  settings = jsonencode({
+    apiHost = "api.segment.io/v1"
+  })
+  slug = "kotlin_android_dev"
+}
