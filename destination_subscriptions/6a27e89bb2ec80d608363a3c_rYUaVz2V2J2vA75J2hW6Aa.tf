c7c9c1436710ea94160af8e536430414d418d4c6
Segment Git Sync <friends@segment.com>
2026-06-09 10:21:33 +0000

    Destination Subscription 'rYUaVz2V2J2vA75J2hW6Aa' created by cortega@twilio.com

---
+++ destination_subscriptions/6a27e89bb2ec80d608363a3c_rYUaVz2V2J2vA75J2hW6Aa.tf
@@ -0,0 +1,36 @@
+import {
+  to = segment_destination_subscription.id-6a27e89bb2ec80d608363a3c_rYUaVz2V2J2vA75J2hW6Aa
+  id = "6a27e89bb2ec80d608363a3c:rYUaVz2V2J2vA75J2hW6Aa"
+}
+
+resource "segment_destination_subscription" "id-6a27e89bb2ec80d608363a3c_rYUaVz2V2J2vA75J2hW6Aa" {
+  action_id      = "oiFvX4YXwyaPVysi1t7mtz"
+  destination_id = "6a27e89bb2ec80d608363a3c"
+  enabled        = false
+  model_id       = "fCPotxBPWAkEMCg1ATSuLr"
+  name           = "Send Track"
+  reverse_etl_schedule = {
+    config = jsonencode({
+      interval = "1h"
+    })
+    strategy = "PERIODIC"
+  }
+  settings = jsonencode({
+    consent = {
+      "@path" = "$.context.consent"
+    }
+    event_name = "hardcoded_event"
+    message_id = {
+      "@path" = "$.messageId"
+    }
+    traits = {
+      retl_track_trait_01 = {
+        "@template" = "{{properties.TRAIT_NAME}}:{{properties.TRAIT_VALUE}}"
+      }
+    }
+    user_id = {
+      "@path" = "$.__segment_id"
+    }
+  })
+  trigger = "event = \"new\" or event = \"updated\""
+}
