27dac164e557269043bf7050412f816973d56432
Segment Git Sync <friends@segment.com>
2026-06-11 04:38:05 +0000

    Destination Subscription 'f4c7HjVLMxzsNLkRaNztRh' created by cortega@twilio.com

---
+++ destination_subscriptions/6296432cb9439c1428d95f4d_f4c7HjVLMxzsNLkRaNztRh.tf
@@ -0,0 +1,37 @@
+import {
+  to = segment_destination_subscription.id-6296432cb9439c1428d95f4d_f4c7HjVLMxzsNLkRaNztRh
+  id = "6296432cb9439c1428d95f4d:f4c7HjVLMxzsNLkRaNztRh"
+}
+
+resource "segment_destination_subscription" "id-6296432cb9439c1428d95f4d_f4c7HjVLMxzsNLkRaNztRh" {
+  action_id            = "tNPRvBDTsfszvJQy6kE9K3"
+  destination_id       = "6296432cb9439c1428d95f4d"
+  enabled              = false
+  model_id             = null
+  name                 = "Custom Mobile Event - PLTV"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    clientId = {
+      "@if" = {
+        else = {
+          "@path" = "$.anonymousId"
+        }
+        exists = {
+          "@path" = "$.userId"
+        }
+        then = {
+          "@path" = "$.userId"
+        }
+      }
+    }
+    data_stream_type     = "Web"
+    engagement_time_msec = 1
+    name = {
+      "@path" = "$.event"
+    }
+    timestamp_micros = {
+      "@path" = "$.timestamp"
+    }
+  })
+  trigger = "type = \"track\""
+}
