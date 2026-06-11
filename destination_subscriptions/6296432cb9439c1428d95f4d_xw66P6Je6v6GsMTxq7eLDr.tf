72cda398225fe4c4ef259a6327c196792cdfb88e
Segment Git Sync <friends@segment.com>
2026-06-11 04:38:09 +0000

    Destination Subscription 'xw66P6Je6v6GsMTxq7eLDr' modified by cortega@twilio.com

--- destination_subscriptions/6296432cb9439c1428d95f4d_xw66P6Je6v6GsMTxq7eLDr.tf
+++ destination_subscriptions/6296432cb9439c1428d95f4d_xw66P6Je6v6GsMTxq7eLDr.tf
@@ -4,11 +4,36 @@ import {
 }
 
 resource "segment_destination_subscription" "id-6296432cb9439c1428d95f4d_xw66P6Je6v6GsMTxq7eLDr" {
-  action_id      = "21Vhf5XWiD5aLnzvq4k9yy"
-  destination_id = "6296432cb9439c1428d95f4d"
-  enabled        = true
-  model_id       = null
-  name           = "Page View"
-  settings       = "{\"clientId\":{\"@if\":{\"else\":{\"@path\":\"$.anonymousId\"},\"exists\":{\"@path\":\"$.userId\"},\"then\":{\"@path\":\"$.userId\"}}},\"engagement_time_msec\":1,\"page_location\":{\"@path\":\"$.context.page.url\"},\"page_referrer\":{\"@path\":\"$.context.page.referrer\"},\"page_title\":{\"@path\":\"$.context.page.title\"}}"
-  trigger        = "type = \"page\""
+  action_id            = "21Vhf5XWiD5aLnzvq4k9yy"
+  destination_id       = "6296432cb9439c1428d95f4d"
+  enabled              = false
+  model_id             = null
+  name                 = "Page View"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    clientId = {
+      "@if" = {
+        else = {
+          "@path" = "$.anonymousId"
+        }
+        exists = {
+          "@path" = "$.userId"
+        }
+        then = {
+          "@path" = "$.userId"
+        }
+      }
+    }
+    engagement_time_msec = 1
+    page_location = {
+      "@path" = "$.context.page.url"
+    }
+    page_referrer = {
+      "@path" = "$.context.page.referrer"
+    }
+    page_title = {
+      "@path" = "$.context.page.title"
+    }
+  })
+  trigger = "type = \"page\""
 }
