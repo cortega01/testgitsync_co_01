034bdb72c9ebe0def85c90786cd1054d44b9fdc5
Segment Git Sync <friends@segment.com>
2026-06-12 06:54:17 +0000

    Source 'gPzpYsUz58fSYyhANRE8do' created by cortega@twilio.com

---
+++ sources/gPzpYsUz58fSYyhANRE8do.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-gPzpYsUz58fSYyhANRE8do
+  id = "gPzpYsUz58fSYyhANRE8do"
+}
+
+resource "segment_source" "id-gPzpYsUz58fSYyhANRE8do" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "iUM16Md8P2"
+  }
+  name     = "rETL GA4 Mobile Event Stream"
+  settings = jsonencode({})
+  slug     = "retl_ga4_mobile_event_stream"
+}
