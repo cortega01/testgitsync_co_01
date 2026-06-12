7bcf52a7eaaa09dcf07251aa01bbd853abc03f0c
Segment Git Sync <friends@segment.com>
2026-06-12 06:26:07 +0000

    Destination Subscription '4PdWYiq1Wjox7j2CgrGjrS' created by cortega@twilio.com

---
+++ destination_subscriptions/69322a7307f444425e48ddb1_4PdWYiq1Wjox7j2CgrGjrS.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_destination_subscription.id-69322a7307f444425e48ddb1_4PdWYiq1Wjox7j2CgrGjrS
+  id = "69322a7307f444425e48ddb1:4PdWYiq1Wjox7j2CgrGjrS"
+}
+
+resource "segment_destination_subscription" "id-69322a7307f444425e48ddb1_4PdWYiq1Wjox7j2CgrGjrS" {
+  action_id            = "nFPnRozhz1mh4Gbx4MLvT5"
+  destination_id       = "69322a7307f444425e48ddb1"
+  enabled              = false
+  model_id             = null
+  name                 = "Send"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    batch_keys = ["url", "method", "headers"]
+    data = {
+      "@path" = "$."
+    }
+    method = "POST"
+    url    = "https://webhooksite.net/f648d7e3-1bdc-4453-b265-90a65b2eee79"
+  })
+  trigger = "type = \"track\" and properties.function_enriched = true"
+}
