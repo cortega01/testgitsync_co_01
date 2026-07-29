729cdf4401a05a84dcf6580f4375fb85739a96b1
Segment Git Sync <friends@segment.com>
2026-07-29 05:01:00 +0000

    Destination '6a69890b2e9ce59d9b66837b' created by cortega@twilio.com

---
+++ destinations/6a69890b2e9ce59d9b66837b.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-6a69890b2e9ce59d9b66837b
+  id = "6a69890b2e9ce59d9b66837b"
+}
+
+resource "segment_destination" "id-6a69890b2e9ce59d9b66837b" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "6a6987b07f901bef1c86ad4a"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name      = "Eloqua Destination Function (Twilio-Cortega)"
+  settings  = jsonencode({})
+  source_id = "7U1pz1W1ESiasVbLSGWWzT"
+}
