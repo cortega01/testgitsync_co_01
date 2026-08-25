f3e01bf70c94ce4f0b7a66f62fa3b3853996cc24
Segment Git Sync <friends@segment.com>
2026-08-25 13:56:24 +0000

    Source '6zf9s5bDh3mrdrYKgGENya' created by cortega@twilio.com

---
+++ sources/6zf9s5bDh3mrdrYKgGENya.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-6zf9s5bDh3mrdrYKgGENya
+  id = "6zf9s5bDh3mrdrYKgGENya"
+}
+
+resource "segment_source" "id-6zf9s5bDh3mrdrYKgGENya" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "lI8p2ldOqa"
+  }
+  name     = "Iterable Source"
+  settings = jsonencode({})
+  slug     = "iterable_source"
+}
